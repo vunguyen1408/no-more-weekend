306eba5f46bec3cdce782e2d6344cc5ccab03887
japonikaa <japonikaa@gmail.com>
2017-06-13 17:03:21 +0700

    update

--- online_marketing_tool/label_csv.py
+++ online_marketing_tool/label_csv.py
@@ -171,7 +171,11 @@ def parse_ads_creatives_csv_to_json(pdate):
     #Dev env
     #base_dir="/home/leth/Workspace/Python/python3/parse_csv/sources/"
     #Prod env
-    base_dir="/u01/oracle/oradata/APEX/MARKETING_TOOL_02/"
+    if pdate < "2017-05-01":
+        base_dir="/u01/oracle/oradata/APEX/MARKETING_TOOL_02/backup/"
+    else:
+        base_dir="/u01/oracle/oradata/APEX/MARKETING_TOOL_02/"
+        
     base_dir_json="/u01/oracle/oradata/APEX/MARKETING_TOOL_02_JSON/"
 
     wrk_dir=os.path.join(base_dir, pdate)
