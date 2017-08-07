3b182efd6cf320e9f6be59596b4f4c3949e4fd79
ThanhDuon2712 <ltduong1994@gmail.com>
2017-08-07 10:15:48 +0700

    Update file label_update

--- online_marketing_tool/label_update.py
+++ online_marketing_tool/label_update.py
@@ -460,27 +460,192 @@ def label_ads_creatives_json_audit_content(pdate):
         print("Wrong file or file path")
 
 
+def get_json3(list_file):
+    import csv
+    import json
+    import pandas as pd
+    import re
+    from pprint import pprint
+    import time
 
+    frame = pd.DataFrame()
+    list_json = []
+
+    for files_ in list_file:
+        with open(files_) as rawfile:
+            for row in rawfile:
+                data = row.split(", ",1) # split by first comma
+
+                #get ads_id
+                pattern =  "(\d+)"
+                matchObject = re.search(pattern, data[0], flags=0)
+                ads_id=matchObject.group()
+
+                #get json
+                json_data = json.loads(data[1])
+                json_data[0]['ad_id']=ads_id # bo sung ads_id vao json
+                #print(json_data[0]["ad_id"])
+                list_json.append(json_data[0])
+
+
+
+    #print(list_[0]["body"])
+    #print(list_json[0]["body"],list_json[0]["image_url"])
+
+
+def parse_ads_creatives_csv_to_json(pdate):
+
+    import os, os.path
+    #from os.path import splitext, basename, join
+    import io
+    import json
+    import time
+
+    #Dev env
+    #base_dir="/home/leth/Workspace/Python/python3/parse_csv/sources/"
+    #Prod env
+    if pdate < "2017-05-01":
+        base_dir="/u01/oracle/oradata/APEX/MARKETING_TOOL_02/backup/"
+    else:
+        base_dir="/u01/oracle/oradata/APEX/MARKETING_TOOL_02/"
+
+    base_dir_json="/u01/oracle/oradata/APEX/MARKETING_TOOL_02_JSON/"
+
+    wrk_dir=os.path.join(base_dir, pdate)
+    json_dir=os.path.join(base_dir_json, pdate)
+
+    if not os.path.exists(json_dir):
+        os.makedirs(json_dir)
+
+    ads_creatives_file_name = "ads_creatives_"+pdate+".json"
+    ads_creatives_file= os.path.join(json_dir, ads_creatives_file_name)
+    list_file = []
+    list_json = []
+
+
+    for root, dirs, files in os.walk(wrk_dir):
+        for f in files:
+            fullpath = os.path.join(root, f)
+
+            #if os.path.splitext(fullpath)[1] == '.txt':
+            if 'creatives.txt' in fullpath:
+                print (fullpath)
+                #get_json(fullpath)
+                list_file.append(fullpath)
+
+    #print(list_files)
+    list_json = get_json3(list_file)
+
+    final_json={}
+    final_json['my_json']=list_json
+    with open (ads_creatives_file,'w') as f:
+        json.dump(final_json,f)
+
+def parse_ads_creatives_json_audit_content(pdate):
+
+    import os, os.path
+    #from os.path import splitext, basename, join
+    import io
+    import json
+    import time
+
+
+    #Dev env
+    #base_dir="/home/leth/Workspace/Python/python3/parse_csv/sources/"
+    #Prod env
+    base_dir="/u01/oracle/oradata/APEX/MARKETING_TOOL_02_JSON/"
+    wrk_dir=os.path.join(base_dir, pdate)
+
+    ads_creatives_file_name = "ads_creatives_"+pdate+".json"
+    ads_creatives_audit_content_file_name = "ads_creatives_audit_content_"+pdate+".json"
+
+    ads_creatives_audit_content_file= os.path.join(wrk_dir, ads_creatives_audit_content_file_name)
+
+    list_file = []
+    list_json = []
+
+    #audit content object
+    list_audit_context = ['image_url','thumbnail_url','link','video_id','message']
+
+    #find all file
+    for root, dirs, files in os.walk(wrk_dir):
+        for f in files:
+            fullpath = os.path.join(root, f)
+
+            #if os.path.splitext(fullpath)[1] == '.txt':
+            if ads_creatives_file_name in fullpath:
+                #print (fullpath)
+                #get_json(fullpath)
+                list_file.append(fullpath)
+
+    #get all data
+    for file_ in list_file:
+        with open (file_,'r') as file_json:
+            reader=json.load(file_json)
+            for row in reader['my_json']:
+                list_json.append(row)
+                #print(row["image_url"])
+
+
+    #get audit content
+    position=0
+    for i in list_json:
+        #print(json["image_url"])
+        #print(type(i[]))
+        audit_content={}
+        #get content
+        for j in list_audit_context:
+
+            audit_content_object=j+'s' #name
+            #print(audit_content_object)
+            audit_content[audit_content_object]=[]
+
+
+            #for k in _finditem2(i[0],j):
+            for k in _finditem2(i,j):
+                #add dict content
+                content = {}
+                content[j] = k
+                #print(k)
+
+                audit_content[audit_content_object].append(content)
+
+        #print(type(list_json[position][0]))
+        #print(type(list_json[i]))
+        #list_json[position][0]['audit_content']=audit_content
+        list_json[position]['audit_content']=audit_content
+        position+=1
+
+
+    final_json={}
+    final_json['my_json']=list_json
+    #print(list_json[0][0]['audit_content']['image_urls'])
+    with open (ads_creatives_audit_content_file,'w') as f:
+        json.dump(final_json,f)
 
 
 
 
 # Lua chon chay tung ngay
 
-# def main(pdate):
-#     """Run a label request """
-#     vdate=pdate
-#     label_ads_creatives_json_audit_content(vdate)
-#
-#
-#
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('pdate', help='The date you\'d like to label.')
-#     args = parser.parse_args()
-#     g_vdate=args.pdate
-#     # print(g_vdate)
-#     main(g_vdate)
+def main(pdate):
+    """Run a label request """
+
+    vdate=pdate
+    parse_ads_creatives_csv_to_json(vdate)
+    #analyze_ads_creatives_json(vdate)
+    parse_ads_creatives_json_audit_content(vdate)
+    label_ads_creatives_json_audit_content(vdate)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pdate', help='The date you\'d like to label.')
+    args = parser.parse_args()
+    g_vdate=args.pdate
+    print(g_vdate)
+    main(g_vdate)
 
 ##############################################################
 # delete all file list image_label
@@ -499,16 +664,16 @@ def label_ads_creatives_json_audit_content(pdate):
 
 ###########################################################
 
-# Lua chon chay full
-def main():
-    """Run a label request """
-    list_folder = next(os.walk(path_base))[1]
-    sorted(list_folder)
-    for date in list_folder:
-        print (date)
-        label_ads_creatives_json_audit_content(date)
+# # Lua chon chay full
+# def main():
+#     """Run a label request """
+#     list_folder = next(os.walk(path_base))[1]
+#     sorted(list_folder)
+#     for date in list_folder:
+#         print (date)
+#         label_ads_creatives_json_audit_content(date)
 
-    # date = '2016-10-02'
-    # label_ads_creatives_json_audit_content(date)
+#     # date = '2016-10-02'
+#     # label_ads_creatives_json_audit_content(date)
 
-main()
+# main()
