2e51c3d64373cee6784e5b37e2686c3c18d3b6e4
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-13 17:54:21 +0700

    Update connect database

--- adwords_python3/online_marketing/connect_data_base.py
+++ adwords_python3/online_marketing/connect_data_base.py
@@ -1,6 +1,6 @@
 import cx_Oracle
 import json
-from datetime import datetime , timedelta, date
+import datetime
 
 conn = cx_Oracle.connect('MARKETING_TOOL_02/MARKETING_TOOL_02_9999@10.60.1.42:1521/APEX42DEV')
 cursor = conn.cursor()
