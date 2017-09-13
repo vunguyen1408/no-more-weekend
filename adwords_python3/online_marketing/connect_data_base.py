63521712d3fc4f5da3deaa8d445f56c916c583b6
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-13 16:27:12 +0700

    Update connect database

--- adwords_python3/online_marketing/connect_data_base.py
+++ adwords_python3/online_marketing/connect_data_base.py
@@ -2,3 +2,9 @@ import cx_Oracle
 
 conn = cx_Oracle.connect('MARKETING_TOOL_02/MARKETING_TOOL_02_9999@10.60.1.42:1521/APEX42DEV')
 cursor = conn.cursor()
+
+query = 'select * from MARKETING_TOOL_02.PYTHON_STG_TEST_01'
+cursor.execute(query)
+
+row = cursor.fetchall()
+print (row)
