2d2de4e7df5cdd0ebbaf5ff7017f9300179b74df
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-13 23:03:33 +0700

    Update connect database

--- adwords_python3/online_marketing/connect_data_base.py
+++ adwords_python3/online_marketing/connect_data_base.py
@@ -5,20 +5,14 @@ import datetime
 conn = cx_Oracle.connect('MARKETING_TOOL_02/MARKETING_TOOL_02_9999@10.60.1.42:1521/APEX42DEV')
 cursor = conn.cursor()
 
-query = 'select * from STG_FA_DATA_GG'
+query = 'select Year, month, Legal, Department, Dept, Product, Reason code-Oracle, Eform No, Start Day, End Day-Estimate, \
+                Channel, Eform Type, Unit _Option, \
+                Unit cost, Amount USD, Value, \
+		Engagement, Impressions, Click, Views, Install, NRU from STG_FA_DATA_GG'
 cursor.execute(query)
 
 row = cursor.fetchall()
 temp = list(row)
 unmap = {}
-print (len(row))
-for i in temp:
-	for j in i:
-		if isinstance(j, datetime.datetime):
-			print (type(j))
-			j = j.strftime('%Y-%m-%d')
-			print (type(j))
-unmap['plan'] = temp
-
-with open ('plan.json','w') as f:
+print (row)
 	json.dump(unmap, f) 
