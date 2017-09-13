db796893258a9be5cfdaffc6013c506007166d25
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-13 17:28:02 +0700

    Update connect data

--- adwords_python3/online_marketing/connect_data_base.py
+++ adwords_python3/online_marketing/connect_data_base.py
@@ -1,10 +1,14 @@
 import cx_Oracle
+import json
 
 conn = cx_Oracle.connect('MARKETING_TOOL_02/MARKETING_TOOL_02_9999@10.60.1.42:1521/APEX42DEV')
 cursor = conn.cursor()
 
-query = 'select * from MARKETING_TOOL_02.PYTHON_STG_TEST_01'
+query = 'select * from STG_FA_DATA_GG'
 cursor.execute(query)
 
 row = cursor.fetchall()
+unmap['plan'] = row
+with open ('plan.json','w') as f:
+	json.dump(unmap, f)
 print (row)
