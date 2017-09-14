324c2e825aab1022cd1b952bb09f6f2239188f43
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-14 11:00:39 +0700

    Update connect database

--- adwords_python3/online_marketing/connect_data_base.py
+++ adwords_python3/online_marketing/connect_data_base.py
@@ -5,10 +5,12 @@ import datetime
 conn = cx_Oracle.connect('MARKETING_TOOL_02/MARKETING_TOOL_02_9999@10.60.1.42:1521/APEX42DEV')
 cursor = conn.cursor()
 
-query = 'select Year, month, Legal, Department, Dept, Product, Reason code-Oracle, Eform No, Start Day, End Day-Estimate, \
-                Channel, Eform Type, Unit _Option, \
-                Unit cost, Amount USD, Value, \
-		Engagement, Impressions, Click, Views, Install, NRU from STG_FA_DATA_GG'
+# query = 'select Year, month, Legal, Department, Dept, Product, Reason code-Oracle, Eform No, Start Day, End Day-Estimate, \
+#                 Channel, Eform Type, Unit _Option, \
+#                 Unit cost, Amount USD, Value, \
+# 		Engagement, Impressions, Click, Views, Install, NRU from STG_FA_DATA_GG'
+
+query = 'select * from STG_FA_DATA_GG'
 cursor.execute(query)
 
 row = cursor.fetchall()
