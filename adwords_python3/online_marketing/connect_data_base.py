8d62a983fb6cd3165464d333bbe102140e721fc7
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-14 14:30:20 +0700

    update connect_database

--- adwords_python3/online_marketing/connect_data_base.py
+++ adwords_python3/online_marketing/connect_data_base.py
@@ -1,3 +1,37 @@
+# import cx_Oracle
+# import json
+# import datetime
+
+# #============================== Connect database =============================
+# conn = cx_Oracle.connect('MARKETING_TOOL_02/MARKETING_TOOL_02_9999@10.60.1.42:1521/APEX42DEV')
+# cursor = conn.cursor()
+
+# #======================= Get data from database ==============================
+# query = 'select CYEAR, CMONTH, LEGAL, DEPARTMENT, DEPARTMENT_NAME, PRODUCT, REASON_CODE_ORACLE, EFORM_NO, \
+# 				START_DAY, END_DAY_ESTIMATE, CHANNEL, EFORM_TYPE, UNIT_OPTION, UNIT_COST, AMOUNT_USD, CVALUE, \
+# 				ENGAGEMENT, IMPRESSIONS, CLIKE, CVIEWS, INSTALL, NRU, INSERT_DATE \
+# 		from STG_FA_DATA_GG'
+
+# cursor.execute(query)
+# row = cursor.fetchall()
+# temp = list(row)
+
+# #===================== Convert data into json =================================
+# unmap = {}
+# list_key = ['CYEAR', 'CMONTH', 'LEGAL', 'DEPARTMENT', 'DEPARTMENT_NAME', 'PRODUCT', 
+# 			'REASON_CODE_ORACLE', 'EFORM_NO', 'START_DAY', 'END_DAY_ESTIMATE', 'CHANNEL', 
+# 			'FORM_TYPE', 'UNIT_OPTION', 'UNIT_COST', 'AMOUNT_USD', 'CVALUE', 'ENGAGEMENT', 
+# 			'IMPRESSIONS', 'CLIKE', 'CVIEWS', 'INSTALL', 'NRU', 'INSERT_DATE']
+
+# list_temp = []
+# for plan in temp:
+# 	for value in plan:
+# 		if (value.isdigit() or value.replace(".", "").isdigit()):
+# 			value = float(value)
+# 		if isinstance(value, datetime.datetime):            
+#             value = value.strftime('%Y-%m-%d')
+# print (temp)
+
 import cx_Oracle
 import json
 import datetime
@@ -15,19 +49,59 @@ query = 'select CYEAR, CMONTH, LEGAL, DEPARTMENT, DEPARTMENT_NAME, PRODUCT, REAS
 cursor.execute(query)
 row = cursor.fetchall()
 temp = list(row)
+#print (row)
+
 
 #===================== Convert data into json =================================
-unmap = {}
+
 list_key = ['CYEAR', 'CMONTH', 'LEGAL', 'DEPARTMENT', 'DEPARTMENT_NAME', 'PRODUCT', 
 			'REASON_CODE_ORACLE', 'EFORM_NO', 'START_DAY', 'END_DAY_ESTIMATE', 'CHANNEL', 
 			'FORM_TYPE', 'UNIT_OPTION', 'UNIT_COST', 'AMOUNT_USD', 'CVALUE', 'ENGAGEMENT', 
 			'IMPRESSIONS', 'CLIKE', 'CVIEWS', 'INSTALL', 'NRU', 'INSERT_DATE']
 
-list_temp = []
-for plan in temp:
+list_json= []
+for plan in temp:	
+	list_temp = []
+	unmap = {}
 	for value in plan:
-		if (value.isdigit() or value.replace(".", "").isdigit()):
-			value = float(value)
+		val = value		
 		if isinstance(value, datetime.datetime):            
-            value = value.strftime('%Y-%m-%d')
-print (temp)
+            val = value.strftime('%Y-%m-%d')
+        if (type(value) != 'int') and (value.isdigit() or value.replace(".", "").isdigit()):
+			val = float(value)
+		list_temp.append(val)
+
+	for i in range(len(list_key)):
+		unmap[list_key[i]] = list_temp[i]
+
+	list_json.append(json)
+
+print (list_json)
+
+
+
+
+
+	
+
+# list_temp = []
+# for i in temp:
+#         print (type(i))
+#         print (i)
+#         print ("========================")
+
+
+
+#         for j in i:
+#                 k = j
+#                 if isinstance(j, datetime.datetime):
+#                         print (type(j))
+#                         print (j)
+#                         k = k.strftime('%Y-%m-%d')
+#                         print (type(j))
+#                 list_temp.append(k)
+
+# unmap['plan'] = temp
+
+# with open ('plan.json','w') as f:
+# 	json.dump(unmap, f) 
