ad4e26ea9c29c0b797ac0fce840c72370c3bc5a0
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-14 14:10:17 +0700

    update connect_database

--- adwords_python3/online_marketing/connect_data_base.py
+++ adwords_python3/online_marketing/connect_data_base.py
@@ -2,17 +2,34 @@ import cx_Oracle
 import json
 import datetime
 
+#============================== Connect database =============================
 conn = cx_Oracle.connect('MARKETING_TOOL_02/MARKETING_TOOL_02_9999@10.60.1.42:1521/APEX42DEV')
 cursor = conn.cursor()
 
+#======================= Get data from database ==============================
 query = 'select CYEAR, CMONTH, LEGAL, DEPARTMENT, DEPARTMENT_NAME, PRODUCT, REASON_CODE_ORACLE, EFORM_NO, \
 				START_DAY, END_DAY_ESTIMATE, CHANNEL, EFORM_TYPE, UNIT_OPTION, UNIT_COST, AMOUNT_USD, CVALUE, \
 				ENGAGEMENT, IMPRESSIONS, CLIKE, CVIEWS, INSTALL, NRU, INSERT_DATE \
 		from STG_FA_DATA_GG'
 
 cursor.execute(query)
-
 row = cursor.fetchall()
 temp = list(row)
+#print (row)
+
+
+#===================== Convert data into json =================================
 unmap = {}
-print (row)
+list_key = ['CYEAR', 'CMONTH', 'LEGAL', 'DEPARTMENT', 'DEPARTMENT_NAME', 'PRODUCT', 
+			'REASON_CODE_ORACLE', 'EFORM_NO', 'START_DAY', 'END_DAY_ESTIMATE', 'CHANNEL', 
+			'FORM_TYPE', 'UNIT_OPTION', 'UNIT_COST', 'AMOUNT_USD', 'CVALUE', 'ENGAGEMENT', 
+			'IMPRESSIONS', 'CLIKE', 'CVIEWS', 'INSTALL', 'NRU', 'INSERT_DATE']
+
+list_temp = []
+for plan in temp:
+	for value in plan:
+		if (value.isdigit() or value.replace(".", "").isdigit()):
+			value = float(value)
+		if isinstance(value, datetime.datetime):            
+            value = value.strftime('%Y-%m-%d')
+print (temp)
