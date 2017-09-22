ef32f33a7c14b377757d4489f2f43adbd1e36fb9
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-22 13:11:20 +0700

    Add files via upload

--- adwords_python3/online_marketing/insert_database.py
+++ adwords_python3/online_marketing/insert_database.py
@@ -13,77 +13,34 @@ def InsertDataDate(path_data, connect):
 	statement = 'insert into DTM_GG_PIVOT_DETAIL (SNAPSHOT_DATE, CYEAR, CMONTH, LEGAL, DEPARTMENT, \
 	DEPARTMENT_NAME, PRODUCT, PRODUCT_NAME, REASON_CODE_ORACLE, EFORM_NO, \
 	START_DATE, END_DATE, CHANNEL, UNIT_COST, AMOUNT_USD, \
-	CVALUE, ENGAGEMENT, IMPRESSIONS, REACH, FREQUENCY) \
-	values (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19, :20)'
+	CVALUE, ENGAGEMENT, IMPRESSIONS, REACH, FREQUENCY, \
+	CLIKE, CLICKS_ALL, LINK_CLICKS, CVIEWS, C3S_VIDEO_VIEW, \
+	INSTALL, NRU, EFORM_TYPE, UNIT_OPTION, OBJECTIVE, \
+	EVENT_ID, PRODUCT_ID, CCD_NRU, GG_VIEWS, GG_CONVERSION, \
+	GG_INVALID_CLICKS, GG_ENGAGEMENTS, GG_VIDEO_VIEW, GG_CTR, GG_IMPRESSIONS, \
+	GG_INTERACTIONS, GG_CLICKS, GG_INTERACTION_TYPE, GG_COST, GG_SPEND, \
+	GG_APPSFLYER_INSTALL, GG_STRATEGY_BID_TYPE) \
+	values (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19, :20, \
+	:21, :22, :23, :24, :25, :26, :27, :28, :29, :30, :31, :32, :33, :34, :35, :36, :37, :38, :39, :40, \
+	:41, :42, :43, :44, :45, :46, :47)'
 
 	with open(path_data, 'r') as fi:
 		data = json.load(fi)
 	
-	for value in data['monthly']:
+	for value in data['MONTHLY']:
 		cursor.execute(statement, ('', value['CYEAR'], value['CMONTH'], value['LEGAL'], value['DEPARTMENT'], \
 			value['DEPARTMENT_NAME'], value['PRODUCT'], '', value['REASON_CODE_ORACLE'], value['EFORM_NO'], \
 			datetime.strptime(value['START_DAY'], '%Y-%m-%d'), datetime.strptime(value['END_DAY_ESTIMATE'], '%Y-%m-%d'), \
 			value['CHANNEL'], value['UNIT_COST'], float(value['AMOUNT_USD']), \
-			float(value['CVALUE'], float(value['ENGAGEMENT'], float(value['IMPRESSIONS'], 0, 0))
-
-	
-	
-					
-# 	# -- CLIKE, \        				
-# 	# -- -- CLICKS_ALL, \					
-# 	# -- -- LINK_CLICKS, \					
-# 	# -- CVIEWS, \						
-# 	# -- -- C3S_VIDEO_VIEW, \				
-# 	# -- INSTALL, \						
-# 	# -- NRU, \							
-# 	# -- EFORM_TYPE, \					
-# 	# -- UNIT_OPTION, \					
-# 	# -- -- OBJECTIVE, \					
-# 	# -- -- EVENT_ID, \						
-# 	# -- -- PRODUCT_ID, \					
-# 	# -- -- CCD_NRU, \						
-# 	# -- -- GG_VIEWS, \						
-# 	# -- GG_CONVERSION, \				
-# 	# -- GG_INVALID_CLICKS, \			
-# 	# -- GG_ENGAGEMENTS, \       		
-# 	# -- -- GG_VIDEO_VIEW, \				
-# 	# -- GG_CTR, \						
-# 	# -- GG_IMPRESSIONS, \				
-# 	# -- GG_INTERACTIONS, \				
-# 	# -- GG_CLICKS, \					
-# 	# -- -- GG_INTERACTION_TYPE, \			
-# 	# -- GG_COST, \   					
-# 	# -- GG_SPEND \						
-# 	# -- -- GG_APPSFLYER_INSTALL, \			
-# 	# -- -- GG_STRATEGY_BID_TYPE) \
-# 	# VALUES (:1, :2) '''
-# 	# # , :3, :4, :5, :6, :7
-# 	# # , :9, :10, :11, :12, :13, :14, :15, \
-# 	# # :16, :17, :18, :21, :24, :26, :27, :28, :29, :35, \
-# 	# # :36, :37, :39, :40, :41, :42, :44, :45)'''
-
-# 	# with open(path_data, 'r') as fi:
-# 	# 	data = json.load(fi)
-
-
-# 	# for value in data['monthly']:
-# 	# 	print(value)
-# 	# 	cursor.execute(statement, (value['CYEAR'] + value['CMONTH'], value['CYEAR']))
-# 		# , value['CMONTH'], value['LEGAL'], 
-# 		# 	value['DEPARTMENT'], value['DEPARTMENT_NAME'], value['PRODUCT']))
-# 			# , 
-# 			# value['REASON_CODE_ORACLE'], value['EFORM_NO'], datetime.strptime(value['START_DAY'], '%Y-%m-%d'), 
-# 			# datetime.strptime(value['END_DAY_ESTIMATE'], '%Y-%m-%d'), value['CHANNEL'], value['UNIT_COST'], 
-# 			# float(value['AMOUNT_USD']), float(value['CVALUE']), float(value['ENGAGEMENT']), 
-# 			# float(value['IMPRESSIONS']), float(value['CLIKE']),
-# 			# float(value['CVIEWS']), float(value['INSTALL']), 
-# 			# float(value['NRU']), value['FORM_TYPE'], value['UNIT_OPTION'], 
-# 			# float(value['DATA_MONTHLY']['CONVERSIONS']), float(value['DATA_MONTHLY']['INVALID_CLICKS']), 
-# 			# float(value['DATA_MONTHLY']['ENGAGEMENTS']), float(value['DATA_MONTHLY']['CTR']), 
-# 			# float(value['DATA_MONTHLY']['IMPRESSIONS']), float(value['DATA_MONTHLY']['INTERACTIONS']), 
-# 			# float(value['DATA_MONTHLY']['CLICKS']), 
-# 			# float(value['DATA_MONTHLY']['COST']), float(value['DATA_MONTHLY']['COST']) ))
-
+			float(value['CVALUE']), float(value['ENGAGEMENT']), float(value['IMPRESSIONS']), 0, 0, \
+			float(value['CLIKE']), 0, 0, float(value['CVIEWS']), 0, \
+			float(value['INSTALL']), float(value['NRU']), value['FORM_TYPE'], value['UNIT_OPTION'], '', \
+			'', value['PRODUCT'], 0,  0, float(value['MONTHLY'][0]['DATA_MONTHLY']['CONVERSIONS']), \
+			float(value['MONTHLY'][0]['DATA_MONTHLY']['INVALID_CLICKS']), float(value['MONTHLY'][0]['DATA_MONTHLY']['ENGAGEMENTS']), \
+			0, float(value['MONTHLY'][0]['DATA_MONTHLY']['CTR']), float(value['MONTHLY'][0]['DATA_MONTHLY']['IMPRESSIONS']), \
+			float(value['MONTHLY'][0]['DATA_MONTHLY']['INTERACTIONS']), float(value['MONTHLY'][0]['DATA_MONTHLY']['CLICKS']), \
+			'', float(value['MONTHLY'][0]['DATA_MONTHLY']['COST']), float(value['MONTHLY'][0]['DATA_MONTHLY']['COST']), \
+			0, ''))
 	
 	conn.commit()
 	cursor.close()
@@ -92,7 +49,7 @@ def InsertDataDate(path_data, connect):
 
 
 #path_data = 'D:/WorkSpace/Adwords/Finanlly/AdWords/DATA/PLAN/monthly.json'
-path_data = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/monthly.json'
+path_data = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/monthly2.json'
 InsertDataDate(path_data, connect)
 
 # 	# statement = """INSERT INTO DTM_GG_PIVOT_DETAIL (
