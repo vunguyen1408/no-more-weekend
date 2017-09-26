aad90418935dd46ef3559adeef689cf38d178d5d
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-26 11:38:22 +0700

    Update GG - get install from table

--- adwords_python3/online_marketing/mapping_data/get_install_from_database.py
+++ adwords_python3/online_marketing/mapping_data/get_install_from_database.py
@@ -5,26 +5,29 @@ from datetime import datetime , timedelta, date
 
 
 def GetDataSummaryAppsFlyer(connect, start_date, end_date, media_source, path_file):
-	# ==================== Connect database =======================
-	conn = cx_Oracle.connect(connect)
-	cursor = conn.cursor()
+    # ==================== Connect database =======================
+    conn = cx_Oracle.connect(connect)
+    cursor = conn.cursor()
 
-	statement = "select * from ods_appsflyer where SNAPSHOT_DATE >= '" + start_date \
-	+ "' and SNAPSHOT_DATE <= '" + end_date + "' and MEDIA_SOURCE = '" + media_source +  "'"
-    
-	
-	cursor.execute(statement)
+    statement = statement = "select * from ods_appsflyer where SNAPSHOT_DATE >= to_date('" + start_date \
+    + "', 'mm/dd/yyyy') and SNAPSHOT_DATE <= to_date('" + end_date + "', 'mm/dd/yyyy') and MEDIA_SOURCE = '" + media_source +  "'"
 
-	list_install = cursor.fetchall()
+    cursor.execute(statement)
 
-	for i in list_install:
-		d = str(i[0])
-		temp = d.split('/')
-		d = temp[2] + '-' + temp[0] + '-' + temp[1] 
-		d = str(datetime.strptime(d, '%Y-%m-%d').date())
-		i[0] = str(d)
-	install = {}
-	install['list_install'] = list_install
+    list_install = cursor.fetchall()
+    list_out = []
+    for i in list_install:
+        d = str(i[0])[:10]
+        d = str(datetime.strptimSe(d, '%Y-%m-%d').date())
+        temp = []
+        temp.append(d)
+        temp.append(i[1])
+        temp.append(i[2])
+        temp.append(i[3])
+        list_out.append(temp)
+        print (temp)
+    install = {}
+    install['list_install'] = list_out
 	with open(path_file, 'w') as f:
 		json.dump(install, f)
 
