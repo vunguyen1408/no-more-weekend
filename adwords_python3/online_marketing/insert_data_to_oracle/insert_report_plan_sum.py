a4769d028049468c15403d2d4be67b9cd6001484
thuydungtk <thuydung.cobala@gmail.com>
2017-09-28 17:38:23 +0700

    Add files via upload

--- adwords_python3/online_marketing/insert_data_to_oracle/insert_report_plan_sum.py
+++ adwords_python3/online_marketing/insert_data_to_oracle/insert_report_plan_sum.py
@@ -54,7 +54,10 @@ def ConvertJsonPlanSum(value):
 
 	json_['NET_ACTUAL'] = value['TOTAL_CAMPAIGN']['COST']	 
 	json_['VOLUMN_ACTUAL'] = value['TOTAL_CAMPAIGN']['VOLUMN_ACTUAL']
-	json_['UNIT_COST_ACTUAL'] = float(json_['NET_ACTUAL']) / json_['VOLUMN_ACTUAL']
+	if (json_['VOLUMN_ACTUAL'] == 0):
+		json_['UNIT_COST_ACTUAL'] = None
+	else:
+		json_['UNIT_COST_ACTUAL'] = float(json_['NET_ACTUAL']) / json_['VOLUMN_ACTUAL']
 	json_['APPSFLYER_INSTALL'] = None
 
 	return json_
