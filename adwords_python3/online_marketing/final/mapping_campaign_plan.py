1772510b17880caf3fe4a537cf8953245c1a7bd4
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-29 20:01:15 +0700

    Update

--- adwords_python3/online_marketing/final/mapping_campaign_plan.py
+++ adwords_python3/online_marketing/final/mapping_campaign_plan.py
@@ -62,6 +62,7 @@ def MapAccountWithCampaign(path_folder, list_plan, list_campaign, date):
         camp['Plan'] = None
         camp['STATUS'] = None
       if (camp['Mapping'] == False):  
+        print ("==============================1222222222222222222222222=====================================")
         if (  (eform['PRODUCT_CODE'] != '') and (camp['Campaign'].find(eform['PRODUCT_CODE']) == 0) and \
           (camp['Campaign'].find(str(eform['REASON_CODE_ORACLE'])) >= 0) and \
           (camp['Advertising Channel'].find(str(eform['FORM_TYPE'])) == 0) and \
