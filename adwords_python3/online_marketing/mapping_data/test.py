befdcde2f02a3aa7d16d324ab126bf1bb01e4832
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-30 01:50:44 +0700

    Update Test code

--- adwords_python3/online_marketing/mapping_data/test.py
+++ adwords_python3/online_marketing/mapping_data/test.py
@@ -12,13 +12,23 @@ connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
 
 statement = "select * from DTM_GG_PIVOT_DETAIL"
 
-# ==================== Connect database =======================
-conn = cx_Oracle.connect(connect)
-cursor = conn.cursor()
-cursor.execute(statement)
-log_manual = cursor.fetchall()
-print (log_manual)
+def loadProductAlias(path_data, connect):
+	# ==================== Connect database =======================
+	conn = cx_Oracle.connect(connect)
+	cursor = conn.cursor()
+	statement = 'select PRODUCT_ID, GG_PRODUCT, CCD_PRODUCT from ODS_META_PRODUCT'        
+	cursor.execute(statement)
+	res = list(cursor.fetchall())
+	list_json = []
+	for product in res:
+		json_ = {
+			'PRODUCT_ID': product[0],
+			'GG_PRODUCT': product[1],
+			'CCD_PRODUCT' : product[2]
+		}
+		list_json.append(json_)
+	print (list_json)
 
-# path_data = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/mapping_data'
-# connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
-# ReadTableManualMap(connect, path_data)
+	# with open(path_data, 'w') as fo:
+	#     json.dump(list_json, fo)
+	# cursor.close()
