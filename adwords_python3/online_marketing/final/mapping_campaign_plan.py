6840f356aef231faf4c6569b99ca9051629ee9ed
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-29 19:48:34 +0700

    Update

--- adwords_python3/online_marketing/final/mapping_campaign_plan.py
+++ adwords_python3/online_marketing/final/mapping_campaign_plan.py
@@ -50,6 +50,8 @@ def MapAccountWithCampaign(path_folder, list_plan, list_campaign, date):
     if (camp['Cost'] > 0) and camp['Campaign state'] != 'Total':
       list_campaign_map.append(camp)
 
+  print (len(list_campaign_map))
+  
   for i, eform in enumerate(list_plan):  
     flag = True
     eform['CAMPAIGN'] = []
