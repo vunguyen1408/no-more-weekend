0952c46b6b5f2f08f37d6684191a19fd6475ad3a
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-29 18:47:51 +0700

    Update

--- adwords_python3/online_marketing/final/insert_data_map_to_total.py
+++ adwords_python3/online_marketing/final/insert_data_map_to_total.py
@@ -193,6 +193,7 @@ def CaculatorTotalMonth(plan, date):
 			# So ngay tu start_day den hien tai (co the tren lech 1 ngay)
 			number_date = CaculatorNumberDate(plan['START_DAY'], date)
 			plan['MONTHLY'] = CaculatorListMonth(plan['START_DAY'], date)
+		print (plan['MONTHLY'])
 		for m in plan['MONTHLY']:
 			if m['MONTH'] <= month:
 				# Da co data
