d0a4f10fe8f0f5d3baca6c0167e245a4f7a3b382
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-30 20:25:19 +0700

    update

--- adwords_python3/online_marketing/final/main.py
+++ adwords_python3/online_marketing/final/main.py
@@ -86,7 +86,7 @@ def Daily(connect, path_data, date):
 	#============================== Merge data ===============================
 	print ("\n\n======================= RUN MERGE WITH DATE : " + date + " =========================")
 	merge = time.time()
-	# merge_date.Merge(path_data, list_customer_id, date)
+	merge_date.Merge(path_data, list_customer_id, date)
 	time_merge = time.time() - merge
 	print ("             Time merge: ", time_merge)
 
@@ -94,7 +94,7 @@ def Daily(connect, path_data, date):
 	#============================== Insert data mapping to total ===============================
 	print ("\n\n============= RUN INSERT DATA MAPPING TO TOTAL WITH DATE : " + date + " =================")
 	insert_total = time.time()
-	# insert_to_total.InsertDateToTotal(path_data, date)
+	insert_to_total.InsertDateToTotal(path_data, date)
 	time_insert_total = time.time() - insert_total
 	print ("            Time insert data mapping to total : ", time_insert_total)
 
@@ -111,10 +111,10 @@ def Daily(connect, path_data, date):
 	list_plan_remove = []
 	list_map = []
 	list_camp_remove = []
-	# monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# detail_map.InsertDataMapToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	detail_map.InsertDataMapToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
 
 	time_insert_databse = time.time() - insert_databse
 	print ("            Time insert data to database : ", time_insert_total)
@@ -139,7 +139,7 @@ def ManyDate(connect, path_data, start_date, end_date):
 
 
 start_date = '2017-06-01'
-end_date = '2017-06-01'
+end_date = '2017-06-30'
 path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA'
 connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
 ManyDate(connect, path_data, start_date, end_date)
