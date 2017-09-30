5753689f0eb790f552f7cd08ed18d9e85c7cab54
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-30 20:09:07 +0700

    Update

--- adwords_python3/online_marketing/final/main.py
+++ adwords_python3/online_marketing/final/main.py
@@ -138,8 +138,8 @@ def ManyDate(connect, path_data, start_date, end_date):
 		Daily(connect, path_data, str(d))
 
 
-start_date = '2017-08-31'
-end_date = '2017-08-31'
+start_date = '2017-06-01'
+end_date = '2017-06-01'
 path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA'
 connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
 ManyDate(connect, path_data, start_date, end_date)
