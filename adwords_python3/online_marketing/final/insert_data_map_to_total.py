5e377b4a99278d4eaea9c797b8ac7cb3580ac121
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-30 19:34:55 +0700

    update

--- adwords_python3/online_marketing/final/insert_data_map_to_total.py
+++ adwords_python3/online_marketing/final/insert_data_map_to_total.py
@@ -347,23 +347,24 @@ def GetVolumeActualTotal(plan):
 
 def CreateListPlanMonthly(path_data, date):
 	path_data_total_map = os.path.join(path_data + '/' + str(date) + '/DATA_MAPPING', 'total_mapping' + '.json')
-	with open (path_data_total_map,'r') as f:
-		data_map = json.load(f)
-	for plan in data_map['TOTAL']:
-		plan['TOTAL_CAMPAIGN']['VOLUME_ACTUAL'] = GetVolumeActualTotal(plan)
-		for m in plan['MONTHLY']:
-			m['TOTAL_CAMPAIGN_MONTHLY']['VOLUME_ACTUAL'] = GetVolumeActualMonthly(plan, m)
-		print (plan)
+	if os.path.exists(path_data_total_map):
+		with open (path_data_total_map,'r') as f:
+			data_map = json.load(f)
+		for plan in data_map['TOTAL']:
+			plan['TOTAL_CAMPAIGN']['VOLUME_ACTUAL'] = GetVolumeActualTotal(plan)
+			for m in plan['MONTHLY']:
+				m['TOTAL_CAMPAIGN_MONTHLY']['VOLUME_ACTUAL'] = GetVolumeActualMonthly(plan, m)
+			print (plan)
 
-	sum_ = 0
-	for camp in data_map['UN_CAMPAIGN']:
-		# print (camp)
-		sum_ += camp['Cost']
-	print ("----------- SUM COST : " , sum_)
-	print (len(data_map['MAP']))
-	print (len(data_map['UN_CAMPAIGN']))
-	with open (path_data_total_map,'w') as f:
-		json.dump(data_map, f)
+		sum_ = 0
+		for camp in data_map['UN_CAMPAIGN']:
+			# print (camp)
+			sum_ += camp['Cost']
+		print ("----------- SUM COST : " , sum_)
+		print (len(data_map['MAP']))
+		print (len(data_map['UN_CAMPAIGN']))
+		with open (path_data_total_map,'w') as f:
+			json.dump(data_map, f)
 
 
 def InsertDateToTotal(path_data, date):
