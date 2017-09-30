116665a4f8e5f7c390bb1a5a59ce34003f6f5247
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-30 20:35:50 +0700

    update

--- adwords_python3/online_marketing/final/insert_data_map_to_total.py
+++ adwords_python3/online_marketing/final/insert_data_map_to_total.py
@@ -244,8 +244,8 @@ def MergeDataToTotal(path_data, date):
 		data_total['MAP'] = []
 		data_total['UN_PLAN'] = []
 		data_total['UN_CAMPAIGN'] = []
-	with open (path_data_total_map,'w') as f:
-		json.dump(data_total, f)
+		with open (path_data_total_map,'w') as f:
+			json.dump(data_total, f)
 	if os.path.exists(path_data_map):
 		with open (path_data_map,'r') as f:
 			data_date = json.load(f)
