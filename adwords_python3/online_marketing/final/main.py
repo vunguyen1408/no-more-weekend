223777ece7c1b902165423def25607dfae92c5cc
ThanhDuon2712 <ltduong1994@gmail.com>
2017-09-30 14:11:46 +0700

    update

--- adwords_python3/online_marketing/final/main.py
+++ adwords_python3/online_marketing/final/main.py
@@ -54,13 +54,13 @@ def Daily(connect, path_data, date):
 	#======================== Insert install to data date ==============================
 	print ("\n\n======================= RUN INSERT INSTALL WITH DATE : " + date + " =========================")
 	insert_install = time.time()
-	install.RunInsertInstall(connect, path_data, list_customer_id, date)
+	# install.RunInsertInstall(connect, path_data, list_customer_id, date)
 	time_insert = time.time() - insert_install
 
 	#------------------ Read log manual mapping and get plan ---------------------
-	mapping_data.ReadPlanFromTable(connect, path_data, date)
-	mapping_data.ReadProductAlias(connect, path_data, date)
-	manual.ReadTableManualMap(connect, path_data, date)
+	# mapping_data.ReadPlanFromTable(connect, path_data, date)
+	# mapping_data.ReadProductAlias(connect, path_data, date)
+	# manual.ReadTableManualMap(connect, path_data, date)
 	#----------------------------------------------------------------
 
 	print ("             Time insert install: ", time_insert)
@@ -68,7 +68,7 @@ def Daily(connect, path_data, date):
 	#======================== Mapping data for list account ============================
 	print ("\n\n======================= RUN MAPPING WITH DATE : " + date + " =========================")
 	mapping = time.time()
-	mapping_data.MapDataForAllAccount(list_customer_id, path_data, date)
+	# mapping_data.MapDataForAllAccount(list_customer_id, path_data, date)
 	time_mapping = time.time() - mapping
 	print ("             Time maping: ", time_mapping)
 
@@ -77,7 +77,7 @@ def Daily(connect, path_data, date):
 	#============================== Merge data ===============================
 	print ("\n\n======================= RUN MERGE WITH DATE : " + date + " =========================")
 	merge = time.time()
-	# merge_date.Merge(path_data, list_customer_id, date)
+	merge_date.Merge(path_data, list_customer_id, date)
 	time_merge = time.time() - merge
 	print ("             Time merge: ", time_merge)
 
