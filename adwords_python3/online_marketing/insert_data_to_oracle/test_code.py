bd3ba0e076806527e7828ad9f93b20321736c809
thuydungtk <thuydung.cobala@gmail.com>
2017-10-02 14:25:49 +0700

    Add files via upload

--- adwords_python3/online_marketing/insert_data_to_oracle/test_code.py
+++ adwords_python3/online_marketing/insert_data_to_oracle/test_code.py
@@ -6,12 +6,12 @@ import cx_Oracle
 from datetime import datetime , timedelta, date
 
 
-def ManualFlow(connect, path_data, date):
+def ManualFlow(connect):
 #=================== Read flag running =============================	
 	conn = cx_Oracle.connect(connect)
 	cursor = conn.cursor()
 
-	path = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA/ACCOUNT_ID/TEMP_DATA/2017-08-30/ACCOUNT_ID'
+	path = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA/2017-08-30/ACCOUNT_ID'
 
 	list_folder = next(os.walk(path))[1]
 	for folder in list_folder:	
@@ -28,11 +28,13 @@ def ManualFlow(connect, path_data, date):
 
 	
 				statement = "insert into DTM_GG_RUN_FLAG (FLAG_RUNNING, FINAL_RUNTIME) values (:1, :2)"
+				print('Insert!...')
 				cursor.execute(statement, (name, None))
 				conn.commit()	
+				print('Committed!........')
 	cursor.close()
 
 
 
-path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA'
 connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
+ManualFlow(connect)
