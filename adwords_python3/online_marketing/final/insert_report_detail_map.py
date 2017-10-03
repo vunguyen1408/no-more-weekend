102351daa986efd9f283b66a40e5e458f86c31e5
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-03 12:39:15 +0700

    Update

--- adwords_python3/online_marketing/final/insert_report_detail_map.py
+++ adwords_python3/online_marketing/final/insert_report_detail_map.py
@@ -15,10 +15,11 @@ def InsertDetailUnmap(value, cursor):
 	CVALUE, ENGAGEMENT, IMPRESSIONS, REACH, FREQUENCY, \
 	CLIKE, CLICKS_ALL, LINK_CLICKS, CVIEWS, C3S_VIDEO_VIEW, \
 	INSTALL, NRU, EFORM_TYPE, UNIT_OPTION, OBJECTIVE, \
-	EVENT_ID, PRODUCT_ID, CCD_NRU, GG_VIEWS, GG_CONVERSION, \
+	EVENT_ID, PRODUCT_ID, CCD_NRU, GG_CONVERSION, \
 	GG_INVALID_CLICKS, GG_ENGAGEMENTS, GG_VIDEO_VIEW, GG_CTR, GG_IMPRESSIONS, \
-	GG_INTERACTIONS, GG_CLICKS, GG_INTERACTION_TYPE, GG_COST, GG_SPEND, \
-	GG_APPSFLYER_INSTALL, GG_STRATEGY_BID_TYPE, CAMPAIGN_ID, CAMPAIGN_NAME, UPDATE_DATE) \
+	GG_INTERACTIONS, GG_CLICKS, GG_CAMPAIGN_TYPE, GG_SPEND, \
+	GG_APPSFLYER_INSTALL, GG_STRATEGY_BID_TYPE, CAMPAIGN_ID, CAMPAIGN_NAME, UPDATE_DATE, \
+	GG_MCC_ID, GG_MCC_NAME) \
 	values (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19, :20, \
 	:21, :22, :23, :24, :25, :26, :27, :28, :29, :30, :31, :32, :33, :34, :35, :36, :37, :38, :39, :40, \
 	:41, :42, :43, :44, :45, :46, :47, :48, :49, :50)'	
@@ -29,26 +30,31 @@ def InsertDetailUnmap(value, cursor):
 		value['CVALUE'], value['ENGAGEMENT'], value['IMPRESSIONS'], value['REACH'], value['FREQUENCY'], \
 		value['CLIKE'], value['CLICKS_ALL'], value['LINK_CLICKS'], value['CVIEWS'], value['C3S_VIDEO_VIEW'], \
 		value['INSTALL'], value['NRU'], value['EFORM_TYPE'], value['UNIT_OPTION'], value['OBJECTIVE'], \
-		value['EVENT_ID'], value['PRODUCT_ID'], value['CCD_NRU'], value['GG_VIEWS'], value['GG_CONVERSION'], \
+		value['EVENT_ID'], value['PRODUCT_ID'], value['CCD_NRU'], value['GG_CONVERSION'], \
 		value['GG_INVALID_CLICKS'], value['GG_ENGAGEMENTS'], value['GG_VIDEO_VIEW'], value['GG_CTR'], value['GG_IMPRESSIONS'], \
-		value['GG_INTERACTIONS'], value['GG_CLICKS'], value['GG_INTERACTION_TYPE'], value['GG_COST'], value['GG_SPEND'], \
-		value['GG_APPSFLYER_INSTALL'], value['GG_STRATEGY_BID_TYPE'], value['CAMPAIGN_ID'], value['CAMPAIGN_NAME'], value['UPDATE_DATE']))	
+		value['GG_INTERACTIONS'], value['GG_CLICKS'], value['GG_CAMPAIGN_TYPE'], value['GG_SPEND'], \
+		value['GG_APPSFLYER_INSTALL'], value['GG_STRATEGY_BID_TYPE'], value['CAMPAIGN_ID'], value['CAMPAIGN_NAME'], value['UPDATE_DATE'], \
+		value['GG_MCC_ID'], value['GG_MCC_NAME']))	
 	
 	print("A row inserted!.......")
 
 
-def SelectMap(cursor):
+def SelectDetailUnmap(cursor):
 	#==================== Insert data into database =============================
-	statement = 'select SNAPSHOT_DATE, CAMPAIGN_ID, PRODUCT, REASON_CODE_ORACLE, \
-	EFORM_NO, EFORM_TYPE, UNIT_OPTION from DTM_GG_PIVOT_DETAIL_UNMAP'	
+	statement = 'Select SNAPSHOT_DATE, CAMPAIGN_ID, \
+	PRODUCT, REASON_CODE_ORACLE, EFORM_TYPE, UNIT_OPTION \
+	from DTM_GG_PIVOT_DETAIL_UNMAP'
 		
-	cursor.execute(statement)
-	list_data = cursor.fetchall()
-	return list_data
+	cursor.execute(statement)	
+	list_unmap = list(cursor.fetchall())
+	
+	return list_unmap
+	
 
 
 def ConvertJsonPlan(value):
 	json_ = {}	
+
 	json_['CYEAR'] = '20' + value['CYEAR']
 	if (len(value['CMONTH']) == 1):
 		json_['CMONTH'] = '0' + value['CMONTH']
@@ -114,8 +120,7 @@ def ConvertJsonPlan(value):
 
 	json_['EVENT_ID'] = value['REASON_CODE_ORACLE']
 	json_['PRODUCT_ID'] = value['PRODUCT']
-	json_['CCD_NRU'] = None
-	json_['GG_VIEWS'] = None
+	json_['CCD_NRU'] = None	
 	json_['GG_CONVERSION'] = None
 	json_['GG_INVALID_CLICKS'] = None
 	json_['GG_ENGAGEMENTS'] = None
@@ -125,8 +130,7 @@ def ConvertJsonPlan(value):
 
 	json_['GG_INTERACTIONS'] = None
 	json_['GG_CLICKS'] = None
-	json_['GG_INTERACTION_TYPE'] = ''
-	json_['GG_COST'] = None
+	json_['GG_CAMPAIGN_TYPE'] = ''	
 	json_['GG_SPEND'] = None
 
 	json_['GG_APPSFLYER_INSTALL'] = None
@@ -134,19 +138,10 @@ def ConvertJsonPlan(value):
 	json_['CAMPAIGN_ID'] = None
 	json_['CAMPAIGN_NAME'] = None
 	json_['UPDATE_DATE'] = None
+	json_['GG_MCC_ID'] = None
+	json_['GG_MCC_NAME'] = None
 
 	return json_
-
-def SelectDetailUnmap(cursor):
-	#==================== Insert data into database =============================
-	statement = 'Select SNAPSHOT_DATE, CAMPAIGN_ID, \
-	PRODUCT, REASON_CODE_ORACLE, EFORM_TYPE, UNIT_OPTION \
-	from DTM_GG_PIVOT_DETAIL_UNMAP'
-		
-	cursor.execute(statement)	
-	list_unmap = list(cursor.fetchall())
-	
-	return list_unmap
 
 def getProductID(value):
 	file_product = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/insert_data_to_oracle/product.xlsx'
@@ -162,6 +157,7 @@ def getProductID(value):
 
 def ConvertJsonCamp(value):
 	json_ = {}	
+
 	json_['SNAPSHOT_DATE'] = value['Date']
 	json_['CYEAR'] = value['Date'][0:4]
 	json_['CMONTH'] = value['Date'][5:7]
@@ -200,9 +196,8 @@ def ConvertJsonCamp(value):
 	json_['OBJECTIVE'] = None
 
 	json_['EVENT_ID'] = None
-	json_['PRODUCT_ID'] = None
-	json_['CCD_NRU'] = None
-	json_['GG_VIEWS'] = value['Views']
+	json_['PRODUCT_ID'] = str(getProductID(value))
+	json_['CCD_NRU'] = None	
 	json_['GG_CONVERSION'] = value['Conversions']
 
 	json_['GG_INVALID_CLICKS'] = value['Invalid clicks']
@@ -213,8 +208,7 @@ def ConvertJsonCamp(value):
 
 	json_['GG_INTERACTIONS'] = value['Interactions']
 	json_['GG_CLICKS'] = value['Clicks']
-	json_['GG_INTERACTION_TYPE'] = ''
-	json_['GG_COST'] = value['Cost']
+	json_['GG_CAMPAIGN_TYPE'] = value['Advertising Channel']	
 	json_['GG_SPEND'] = value['Cost']
 
 	json_['GG_APPSFLYER_INSTALL'] = value['INSTALL']		
@@ -222,12 +216,15 @@ def ConvertJsonCamp(value):
 	json_['CAMPAIGN_ID'] = str(value['Campaign ID'])
 	json_['CAMPAIGN_NAME'] = value['Campaign']
 	json_['UPDATE_DATE'] = datetime.strptime(value['Date'], '%Y-%m-%d')
+	json_['GG_MCC_ID'] = None
+	json_['GG_MCC_NAME'] = None
 
 	return json_
 
 
 def ConvertJsonMap(value):
 	json_ = {}	
+
 	json_['SNAPSHOT_DATE'] = value['Date']
 	json_['CYEAR'] = '20' + value['CYEAR']
 	if (len(value['CMONTH']) == 1):
@@ -293,8 +290,7 @@ def ConvertJsonMap(value):
 
 	json_['EVENT_ID'] = value['REASON_CODE_ORACLE']
 	json_['PRODUCT_ID'] = value['PRODUCT']
-	json_['CCD_NRU'] = None
-	json_['GG_VIEWS'] = value['Views']
+	json_['CCD_NRU'] = None	
 	json_['GG_CONVERSION'] = value['Conversions']
 
 	json_['GG_INVALID_CLICKS'] = value['Invalid clicks']
@@ -305,8 +301,7 @@ def ConvertJsonMap(value):
 
 	json_['GG_INTERACTIONS'] = value['Interactions']
 	json_['GG_CLICKS'] = value['Clicks']
-	json_['GG_INTERACTION_TYPE'] = ''
-	json_['GG_COST'] = value['Cost']
+	json_['GG_CAMPAIGN_TYPE'] = value['Advertising Channel']	
 	json_['GG_SPEND'] = value['Cost']
 
 	json_['GG_APPSFLYER_INSTALL'] = value['INSTALL']		
@@ -314,9 +309,10 @@ def ConvertJsonMap(value):
 	json_['CAMPAIGN_ID'] = str(value['Campaign ID'])
 	json_['CAMPAIGN_NAME'] = value['Campaign']
 	json_['UPDATE_DATE'] = datetime.strptime(value['Date'], '%Y-%m-%d')
+	json_['GG_MCC_ID'] = None
+	json_['GG_MCC_NAME'] = None
 
 	return json_
-
 
 def ReportDetailUnmap(path_data, connect):
 	if os.path.exists(path_data):
