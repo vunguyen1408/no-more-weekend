c26e059820cb1151dcb23506a3aec0a00c7c92ea
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-03 17:42:37 +0700

    update

--- adwords_python3/online_marketing/final/job_manual_mapping.py
+++ adwords_python3/online_marketing/final/job_manual_mapping.py
@@ -1,32 +1,32 @@
-import sys
-import json
-import os
-import time
-from datetime import datetime , timedelta, date
+# import sys
+# import json
+# import os
+# import time
+# from datetime import datetime , timedelta, date
 
 
-import manual_mapping as manual
-import insert_report_monthly_detail as monthly_detail
-import insert_report_monthly_sum as monthly_sum
-import insert_report_plan_sum as plan_sum
-import insert_report_detail_map as detail_map
+# import manual_mapping as manual
+# import insert_report_monthly_detail as monthly_detail
+# import insert_report_monthly_sum as monthly_sum
+# import insert_report_plan_sum as plan_sum
+# import insert_report_detail_map as detail_map
 
 
-def ManualMapping (connect, path_data, date):
-	# =============================== Manual mapping =========================================
-	print ("\n\n============= RUN INSERT MANUAL MAPPING TO TOTAL WITH DATE : " + date + " =================")
-	insert_manual = time.time()
-	list_map, list_plan_remove, list_camp_remove = manual.GetCampaignUnMapForManualMap(connect, path_data, date)
-	time_insert_manual = time.time() - insert_manual
-	# print ("---------- Time insert manual mapping to total : ", time_insert_manual)
+# def ManualMapping (connect, path_data, date):
+# 	# =============================== Manual mapping =========================================
+# 	print ("\n\n============= RUN INSERT MANUAL MAPPING TO TOTAL WITH DATE : " + date + " =================")
+# 	insert_manual = time.time()
+# 	list_map, list_plan_remove, list_camp_remove = manual.GetCampaignUnMapForManualMap(connect, path_data, date)
+# 	time_insert_manual = time.time() - insert_manual
+# 	# print ("---------- Time insert manual mapping to total : ", time_insert_manual)
 	
-	# monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+# 	# monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+# 	# monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+# 	# plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
 
 
 
-date = '2017-08-31'
-path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA'
-connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
-ManualMapping (connect, path_data, date)
+# date = '2017-08-31'
+# path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA'
+# connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
+# ManualMapping (connect, path_data, date)
