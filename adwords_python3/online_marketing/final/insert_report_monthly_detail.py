92b5d155c08293f00ca0304003458ce21891ff0b
thuydungtk <thuydung.cobala@gmail.com>
2017-10-05 11:42:08 +0700

    Update

--- adwords_python3/online_marketing/final/insert_report_monthly_detail.py
+++ adwords_python3/online_marketing/final/insert_report_monthly_detail.py
@@ -141,8 +141,11 @@ def ConvertJsonMonthlyDetail(index, value):
 	json_['OBJECTIVE'] = ''
 
 	json_['EVENT_ID'] = value['REASON_CODE_ORACLE']
-	json_['PRODUCT_ID'] = value['PRODUCT']
-	json_['CCD_NRU'] = value['CCD_NRU']
+	json_['PRODUCT_ID'] = value['PRODUCT']	
+	if (value['CCD_NRU'] is None):
+		json_['CCD_NRU'] = value['CCD_NRU']
+	else:
+		json_['CCD_NRU'] = float(value['CCD_NRU'])
 	json_['GG_VIEWS'] = value['MONTHLY'][index]['TOTAL_CAMPAIGN_MONTHLY']['VIEWS']
 	json_['GG_CONVERSION'] = value['MONTHLY'][index]['TOTAL_CAMPAIGN_MONTHLY']['CONVERSIONS']
 
