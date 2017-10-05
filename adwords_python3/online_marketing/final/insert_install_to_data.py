dfda5c1e2a380ac70a587c8ec19edde3eb08adb0
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-05 16:41:10 +0700

    Update

---
+++ adwords_python3/online_marketing/final/insert_install_to_data.py
@@ -0,0 +1,114 @@
+import sys
+import os
+import pandas as pd
+import cx_Oracle
+import numpy as np
+import json
+from datetime import datetime , timedelta, date
+
+
+def GetDataSummaryAppsFlyer(connect, date, media_source, path_file):
+    # ==================== Connect database =======================
+    conn = cx_Oracle.connect(connect)
+    cursor = conn.cursor()
+
+    day = date[8:]
+    month = date[5:-3]
+    year = date[:4]
+    date = month + '-' + day + '-' + year
+    statement = "select * from ods_appsflyer where SNAPSHOT_DATE \
+    = to_date('" + date + "', 'mm/dd/yyyy') and MEDIA_SOURCE like '" + media_source +  "'"
+
+    cursor.execute(statement)
+
+    list_install = cursor.fetchall()
+    list_out = []
+    for i in list_install:
+        d = str(i[0])[:10]
+        d = str(datetime.strptime(d, '%Y-%m-%d').date())
+        temp = []
+        temp.append(d)
+        temp.append(i[1])
+        temp.append(i[2])
+        temp.append(i[3])
+        temp.append(i[4])
+        temp.append(i[5])
+        temp.append(i[6])
+        list_out.append(temp)
+    install = {}
+    install['list_install'] = list_out
+    with open(path_file, 'w') as f:
+        json.dump(install, f)
+
+
+def ReadDataInstall(path_file_install):
+    with open(path_file_install, 'r') as f:
+        data = json.load(f)
+        # print ("==========================doc duoc file install====================")
+        return data['list_install']
+        
+def AddInstall(path_file, list_install):
+    with open(path_file, 'r') as f:
+        data = json.load(f)
+
+    for campaign in data:
+        if 'INSTALL' not in campaign:
+            campaign['INSTALL'] = 0
+            for install in list_install:
+                if str(campaign['Campaign ID']) == str(install[2]):
+                    campaign['INSTALL'] = install[3]
+    with open(path_file, 'w') as f:
+        json.dump(data, f)
+
+def InsetInstallToDate(path_data, list_install, list_customer_id, date):
+
+    #------------ Creat list install on date ---------------
+    list_install_date = []
+    for install in list_install:
+        if install[0] == date:
+            list_install_date.append(install)
+
+    path_list_account_id = os.path.join(path_data, str(date) + '/ACCOUNT_ID')
+    # list_folder_account = next(os.walk(path_list_account_id))[1]
+    list_folder_account = list_customer_id
+
+    for account in list_folder_account:
+        path_account_id = os.path.join(path_list_account_id, account)
+        if os.path.exists(path_account_id):
+            path_file = os.path.join(path_account_id, 'campaign_' + str (date) + '.json')
+            if os.path.exists(path_file):
+                AddInstall(path_file, list_install_date)
+
+
+def InsetInstall(path_data, path_file_install, start_date, end_date):
+    startDate = datetime.strptime(start_date, '%Y-%m-%d').date()  
+    endDate = datetime.strptime(end_date, '%Y-%m-%d').date() 
+    date = startDate
+
+    while(date <= endDate):
+        # print ("===========================================================")
+        # print (date)
+        list_install = ReadDataInstall(path_file_install)
+        InsetInstallToDate(path_data, list_install, str(date))
+        date = date + timedelta(1)
+
+def RunInsertInstall(connect, path_data, list_customer_id, date):
+    media_source = 'googleadwords_int'
+    path_folder_appsflyer = os.path.join(path_data, str(date) + '/APPS_FLYER')
+    if not os.path.exists(path_folder_appsflyer):
+        os.makedirs(path_folder_appsflyer)
+    path_file = os.path.join(path_folder_appsflyer, 'install.json')
+
+    GetDataSummaryAppsFlyer(connect, str(date), media_source, path_file)
+    list_install = ReadDataInstall(path_file)
+    InsetInstallToDate(path_data, list_install, list_customer_id, date)
+    # print ("================= Insert install campleted ===================")
+
+
+
+# startDate = '2017-06-01'
+# endDate = '2017-06-01'
+# path_data = 'C:/Users/ltduo/Desktop/VNG/DATA/END'
+# path_file_install = 'C:/Users/ltduo/Desktop/VNG/DATA/APP_FLYER/install.json'
+# connect = ''
+# RunInsertInstall(connect, path_data, startDate)
---
+++ adwords_python3/online_marketing/final/insert_report_GG_MCC_List.py
@@ -0,0 +1,99 @@
+import cx_Oracle
+import json
+import os
+from datetime import datetime
+
+	
+
+def InsertMCCList(value, cursor):
+	#==================== Insert data into database =============================
+	statement = 'insert into ODS_GG_ACCOUNT_LIST ( \
+	MCC, MCC_ID, ENTITY, DEPT, STATUS, CONTACT_POINT) \
+	values (:1, :2, :3, :4, :5, :6)'	
+		
+	cursor.execute(statement, (value['MCC'], value['MCC_ID'], \
+		None, value['DEPT'], None, None))
+	
+	print("A row inserted!.......")
+
+def InsertMCCListToDatabase(path_data, connect):
+	#================ Get full account get from Adwords ===============
+	path_mcc  = os.path.join(path_data, 'get_account_MCC_new.json')
+	path_wpl  = os.path.join(path_data, 'get_account_WPL_new.json')
+	list_acc = []
+	list_acc_id = []
+	list_dept_id = []
+
+	with open(path_mcc, 'r') as fi:
+		data = json.load(fi)
+	for value in data:
+		list_acc.append(value['name'])
+		list_acc_id.append(str(value['customerId']))
+		list_dept_id.append(value['deptId'])
+
+	with open(path_wpl, 'r') as fi:
+		data = json.load(fi)
+	for value in data:
+		list_acc.append(value['name'])
+		list_acc_id.append(str(value['customerId']))
+		list_dept_id.append(value['deptId'])
+
+	# ==================== Connect database =======================
+	conn = cx_Oracle.connect(connect)
+	cursor = conn.cursor()
+
+	# ================ Get account from database =================
+	statement = 'select * from ODS_GG_ACCOUNT_LIST'	
+		
+	cursor.execute(statement)
+	res = list(cursor.fetchall())
+	list_mcc_id = ['9247463240', '9719199461']
+	list_dept = ['PG2', 'GS5']
+	for acc in res:
+		acc = list(acc)
+		list_mcc_id.append(acc[1])
+		list_dept.append(acc[3])
+
+
+	for i in range(len(list_acc_id)):
+		if (list_acc_id[i] not in list_mcc_id):
+			if (list_dept_id[i] in list_mcc_id):
+				value = {
+					'MCC': list_acc[i], 
+					'MCC_ID': list_acc_id[i], 
+					'ENTITY': None, 
+					'DEPT': list_dept[list_mcc_id.index(list_dept_id[i])], 
+					'STATUS': None, 
+					'CONTACT_POINT': None
+				}
+			else:
+				value = {
+					'MCC': list_acc[i], 
+					'MCC_ID': list_acc_id[i], 
+					'ENTITY': None, 
+					'DEPT': 'Unidentified', 
+					'STATUS': None, 
+					'CONTACT_POINT': None
+				}
+				
+			print(value)
+			try:
+				InsertMCCList(value, cursor)
+			except UnicodeEncodeError as e:				
+				value['MCC'] = value['MCC'].encode('utf-8')
+				InsertMCCList(value, cursor)
+
+
+
+	conn.commit()
+	print("Committed!.......")
+	cursor.close()
+
+
+
+path_data = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/final/LIST_ACCOUNT'
+connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
+InsertMCCListToDatabase(path_data, connect)
+
+
+
