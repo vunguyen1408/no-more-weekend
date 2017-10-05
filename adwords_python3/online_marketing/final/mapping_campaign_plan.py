1acefa89183f9b1fe0c11ddb13c1dbd95893757e
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-05 15:58:45 +0700

    Update

--- adwords_python3/online_marketing/final/mapping_campaign_plan.py
+++ adwords_python3/online_marketing/final/mapping_campaign_plan.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 import pandas as pd
@@ -119,7 +120,7 @@ def AddProductCode(path_folder, list_plan, date):
     data = json.load(fo)
 
   list_temp = []
-  for plan in list_plan['plan']:
+  for plan in list_plan:
     temp = plan
     temp['PRODUCT_CODE'] = ''
     for alias in data['ALIAS']:
@@ -130,7 +131,7 @@ def AddProductCode(path_folder, list_plan, date):
   # for p in list_temp:
   #   print (p['PRODUCT_CODE'])
   
-  list_plan['plan'] = list_temp
+  list_plan = list_temp
   return list_plan
 
 
@@ -187,10 +188,9 @@ def ReadPlanFromTable(connect, path_folder, date):
   #================ Add product id to plan =================
   ReadProductAlias(connect, path_folder, date)
   nru.ReadNRU(connect, path_folder, date)
-
-  plan_ = AddProductCode(path_folder, plan_, date)
-  plan_ = nru.AddNRU(path_folder, plan_, date)
-  plan_['plan'] = list_json
+  
+  plan_['plan'] = AddProductCode(path_folder, plan_['plan'], date)
+  plan_['plan'] = nru.AddNRU(path_folder, plan_['plan'], date)
   
   with open (file_plan, 'w') as f:
     json.dump(plan_, f)
@@ -211,7 +211,7 @@ def MapData(customer, path_folder, date):
 
   # =============== List plan code ================
   list_plan = ReadPlan(path_folder, date)
-
+  
   #================ Add product id to plan =================
   # list_plan = AddProductCode(path_folder, list_plan, date)
 
