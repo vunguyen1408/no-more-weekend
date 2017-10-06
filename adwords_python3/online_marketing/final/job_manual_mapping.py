c98bbb072c06f5392e7308205740af81a03c1b64
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-06 14:00:22 +0700

    Update

--- adwords_python3/online_marketing/final/job_manual_mapping.py
+++ adwords_python3/online_marketing/final/job_manual_mapping.py
@@ -25,7 +25,7 @@ def ManualMapping (connect, path_data, date):
 	# monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove_unmap, list_camp_remove_unmap, date)
 	# monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove_unmap, list_camp_remove_unmap, date)
 	# plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove_unmap, list_camp_remove_unmap, date)
-	# detail_map.InsertDataMapToDatabase(path_data, connect, list_map, list_plan_remove_unmap, list_camp_remove_unmap, date)
+	detail_map.InsertDataMapToDatabase(path_data, connect, list_map, list_plan_remove_unmap, list_camp_remove_unmap, date)
 
 
 
