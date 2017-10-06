ebd3fcc04c2de2b17d51d3b40afc784724b15af4
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-06 10:36:59 +0700

    Update

--- adwords_python3/online_marketing/final/main.py
+++ adwords_python3/online_marketing/final/main.py
@@ -118,7 +118,7 @@ def Daily(connect, path_data, date):
 	list_camp_remove = []
 	
 	# monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	# monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
 	# plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
 	# detail_map.InsertDataMapToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
 
@@ -148,9 +148,14 @@ def ManyDate(connect, path_data, start_date, end_date):
 	print (" TIME : ", e)
 
 
-start_date = '2017-08-31'
-end_date = '2017-08-31'
+# start_date = '2017-06-01'
+# end_date = '2017-06-30'
 path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA'
 connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
 ManyDate(connect, path_data, start_date, end_date)
 
+if __name__ == '__main__':
+    from sys import argv
+    
+    script, start_date, end_date = argv
+    ManyDate(connect, path_data, start_date, end_date)
