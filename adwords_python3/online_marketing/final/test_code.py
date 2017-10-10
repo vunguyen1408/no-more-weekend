a0b991fad29e0a382e7a621cd6f537d19200246f
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-10 16:35:06 +0700

    Update

--- adwords_python3/online_marketing/final/test_code.py
+++ adwords_python3/online_marketing/final/test_code.py
@@ -1,3 +1,8 @@
 from googleads import adwords
+
+
 adwords_client = adwords.AdWordsClient.LoadFromStorage('/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/googleads.yaml')
+
+acccount_id = '6493618146'
+client.SetClientCustomerId(str(acccount_id))
 campaign_service = adwords_client.GetService('CampaignService', version='v201708')
