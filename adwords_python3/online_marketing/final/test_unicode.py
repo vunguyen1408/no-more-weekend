ae5c7cc17eb0dcc0097c3eec736971b5ca9456c7
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-10 16:30:47 +0700

    Update

--- adwords_python3/online_marketing/final/test_unicode.py
+++ adwords_python3/online_marketing/final/test_unicode.py
@@ -88,7 +88,7 @@
 
 from googleads import adwords
 adwords_client = adwords.AdWordsClient.LoadFromStorage('/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/googleads.yaml')
-campaign_service = client.GetService('CampaignService', version='v201708')
+campaign_service = adwords_client.GetService('CampaignService', version='v201708')
 
 
 
