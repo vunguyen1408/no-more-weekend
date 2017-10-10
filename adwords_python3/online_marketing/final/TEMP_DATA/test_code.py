f736146d32d7d5b41c20e7708a9a03970d5a59d6
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-10 16:54:54 +0700

    Update

--- adwords_python3/online_marketing/final/TEMP_DATA/test_code.py
+++
@@ -1,9 +0,0 @@
-
-
-
-with open('/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/final/log.txt', 'a') as f:
-	import datetime
-	i = datetime.datetime.now()
-	f.write("Current date & time = %s" % i)
-	f.write("==========================================================")
-	f.write("DONNNNNNNNNNNNNNNNNE\n\n")
