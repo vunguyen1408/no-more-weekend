254c6a271c66cf9fe07b60f369d62459c223f46d
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-11 18:39:23 +0700

    Update

--- adwords_python3/online_marketing/final/history_name.py
+++ adwords_python3/online_marketing/final/history_name.py
@@ -113,7 +113,7 @@ def MergerCampList(value, cursor):
 	if (len(res) == 0):
 		InsertCampList(value, cursor)
 	elif (value['CAMPAIGN_NAME'] != res):
-		print (res[0])
+		print (type(res[0]))
 		print ("---------------------------- UPDATE NAME ----------------------------------")
 		UpdateCampList(value, cursor)
 	# print("	A row mergered!.......")
