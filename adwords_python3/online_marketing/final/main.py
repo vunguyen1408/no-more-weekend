e31aed9fc0107376654472b97e44309aeb91400c
thuydungtk <thuydung.cobala@gmail.com>
2017-10-11 17:13:57 +0700

    Update GG

--- adwords_python3/online_marketing/final/main.py
+++ adwords_python3/online_marketing/final/main.py
@@ -27,24 +27,34 @@ def Daily(connect, path_data, date):
 	"""
 
 	# # --------- Doc list account
-	list_customer_id = ['6140218608', '5984796540', '5939646969', '4869930138', '4626844359', \
-						'4047293553', '3581453552', '3320825113', '1954002502', '1547282976', \
-						'8579571740', '8324896471', '7017079687', '7013760267', '6408702983', \
-						'1492278512', '1124503774', '5219026641', '8915969454', '9299123796', \
-						'3381621349', '7986041343', '9377025866', '6102951142', '3668363407', '4895791874', \
-						'1373058452', '3699371994', '1496752295', '6787284625', '4780719992', \
-						'5515537799', '3436962801', '7802963373', '5925380036', '3836577058', \
-						'1163330677', '1057617213', '4798268655', '8812868246', '7976533276', \
-						'9420329501', '3785612315', '9719199461', '1912353902', '4585745870', \
-						'9358928000', '4566721209', '1547282976', '1359687200', '1124503774', \
-						'5219026641', '8760733662', '5460890494', '4270191371', '4219579467', \
-						'3959508668', '1954002502', '6585673574', '5993679244', '5990401446', \
-						'7498338868', '9392975361', '9294243048', '7886422201', '6940796638', \
-						'6942753385', '3818588895', '8640138177', '1493302671', '7539462658', \
-						'5243164713', '9019703669', '3764021980', '8024455693', '7077229774', \
-						'6708858633', '1066457627', '4092061132', '3346913196', '5886101084', \
-						'3752996996', '8353864179', '1033505012', '5008396449', '6319649915', \
-						'1290781574', '1669629424', '6376833586', '6493618146', '9021114325']
+	list_customer_id = ['1033505012', '1057617213', '1066457627', '1124503774', '1163330677', \
+'1276490383', '1290781574', '1359687200', '1373058452', '1492278512', \
+'1493302671', '1496752295', '1547282976', '1669629424', '1912353902', \
+'1954002502', '2789627019', '3320825113', '3346913196', '3381621349', \
+'3436962801', '3581453552', '3668363407', '3699371994', '3752996996', \
+'3764021980', '3785612315', '3818588895', '3836577058', '3959508668', \
+'4047293553', '4092061132', '4219579467', '4270191371', '4566721209', \
+'4585745870', '4626844359', '4798268655', '4869930138', '4888935554', \
+'4895791874', '5008396449', '5219026641', '5243164713', '5460890494', \
+'5477521592', '5515537799', '5558683488', '5879354452', '5881347043', \
+'5886101084', '5925380036', '5939646969', '5984796540', '5990401446', \
+'5993679244', '6102951142', '6140218608', '6223856123', '6319649915', \
+'6376833586', '6408702983', '6493618146', '6585673574', '6708858633', \
+'6787284625', '6940796638', '6942753385', '7011814018', '7013760267', \
+'7017079687', '7077229774', '7498338868', '7539462658', '7802963373', \
+'7886422201', '7976533276', '7986041343', '8024455693', '8324896471', \
+'8353864179', '8559396163', '8579571740', '8640138177', '8760733662', \
+'8812868246', '8915969454', '9019703669', '9021114325', '9294243048', \
+'9299123796', '9358928000', '9377025866', '9392975361', '9420329501', \
+'9694660207', '9719199461', '7891987656', '6267264008', '8583452877', \
+'3099510382', '5471697015', '8198035241', '8919123364', '8934377519', \
+'7906284750', '1670552192', '3785612315', '6507949288', '3752996996', \
+'5515537799', '9280946488', '8897792146', '4732571543', '6319649915', \
+'4845283915', '4963434062', '3950481958', '8977015372', '2018040612', \
+'1237086810', '2474373259', '9203404951', '8628673438', '5957287971', \
+'3265423139', '8483981986', '2686387743', '5930063870', '7061686256', \
+'3994588490', '3769240354', '4227775753', '8003403685', '3061049910', \
+'2395877275', '1849103506', '7000297269', '6233988585', '4018935765']
 
 	start_work_flow = time.time()
 	#========================== Download report =================================
@@ -58,22 +68,22 @@ def Daily(connect, path_data, date):
 	# print ("            Time get report: ", time_download_report)
 
 	#======================== History name ==================================
-	# history.InsertHistoryName(connect, path_data, list_customer_id, date)
+	history.InsertHistoryName(connect, path_data, list_customer_id, date)
 
 	#======================== Insert install to data date ==============================
 	# print ("\n\n======================= RUN INSERT INSTALL WITH DATE : " + date + " =========================")
 	# insert_install = time.time()
-	# install.RunInsertInstall(connect, path_data, list_customer_id, date)
+	install.RunInsertInstall(connect, path_data, list_customer_id, date)
 
 	# time_insert = time.time() - insert_install
 
 
 
 	#------------------ Read log manual mapping and get plan NRU ---------------------
-	# mapping_data.ReadPlanFromTable(connect, path_data, date)
+	mapping_data.ReadPlanFromTable(connect, path_data, date)
 	
-	# mapping_data.ReadProductAlias(connect, path_data, date)
-	# manual.ReadTableManualMap(connect, path_data, date)
+	mapping_data.ReadProductAlias(connect, path_data, date)
+	manual.ReadTableManualMap(connect, path_data, date)
 	#----------------------------------------------------------------
 	# print ("             Time insert install: ", time_insert)
 
@@ -82,7 +92,7 @@ def Daily(connect, path_data, date):
 	#======================== Mapping data for list account ============================
 	# print ("\n\n======================= RUN MAPPING WITH DATE : " + date + " =========================")
 	# mapping = time.time()
-	# mapping_data.MapDataForAllAccount(list_customer_id, path_data, date)
+	mapping_data.MapDataForAllAccount(list_customer_id, path_data, date)
 	# time_mapping = time.time() - mapping
 	# print ("             Time maping: ", time_mapping)
 
@@ -91,7 +101,7 @@ def Daily(connect, path_data, date):
 	#============================== Merge data ===============================
 	# print ("\n\n======================= RUN MERGE WITH DATE : " + date + " =========================")
 	# merge = time.time()
-	# merge_date.Merge(path_data, list_customer_id, date)
+	merge_date.Merge(path_data, list_customer_id, date)
 	# time_merge = time.time() - merge
 	# print ("             Time merge: ", time_merge)
 
@@ -99,7 +109,7 @@ def Daily(connect, path_data, date):
 	#============================== Insert data mapping to total ===============================
 	# print ("\n\n============= RUN INSERT DATA MAPPING TO TOTAL WITH DATE : " + date + " =================")
 	# insert_total = time.time()
-	# insert_to_total.InsertDateToTotal(path_data, date)
+	insert_to_total.InsertDateToTotal(path_data, date)
 	# time_insert_total = time.time() - insert_total
 	# print ("            Time insert data mapping to total : ", time_insert_total)
 
@@ -117,9 +127,9 @@ def Daily(connect, path_data, date):
 	list_map = []
 	list_camp_remove = []
 	
-	# monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
 	detail_map.InsertDataMapToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
 
 	# time_insert_databse = time.time() - insert_databse
