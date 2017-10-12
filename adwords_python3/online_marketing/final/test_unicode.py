1d1ad5168f40d3901deb7e24d047a34df4066576
thuydungtk <thuydung.cobala@gmail.com>
2017-10-12 10:20:45 +0700

    Update GG

--- adwords_python3/online_marketing/final/test_unicode.py
+++ adwords_python3/online_marketing/final/test_unicode.py
@@ -100,45 +100,45 @@ import cx_Oracle
 from datetime import datetime , timedelta, date
 
 def Read_NRU_for_month(cursor, month, product):
-  #==================== Get NRU =============================
-  statement = "Select SNAPSHOT_DATE, PRODUCT_CODE, NRU from STG_NRU where CHANNEL = 'Google' \
-  and  extract (Month from SNAPSHOT_DATE) = :1"
-  cursor.execute(statement, (month))
-  list_NRU = list(cursor.fetchall())  
-  
-  #==================== Get product ID ===================
-  statement = 'Select PRODUCT_ID, CCD_PRODUCT from ODS_META_PRODUCT where PRODUCT_ID = :1'
-  cursor.execute(statement, (product))
-  list_product = list(cursor.fetchall())
+	#==================== Get NRU =============================
+	statement = "Select SNAPSHOT_DATE, PRODUCT_CODE, NRU from STG_NRU where CHANNEL = 'Google' \
+	and  extract (Month from SNAPSHOT_DATE) = :1"
+	cursor.execute(statement, (month))
+	list_NRU = list(cursor.fetchall())  
 
-  ccd_nru = 0  
-  list_nru = []
-  for i in range(len(list_NRU)):
-    list_NRU[i] = list(list_NRU[i])    
-    for pro in list_product:
-      if (list_NRU[i][1] == pro[1]):
-      	data = [list_NRU[i][0], list_NRU[i][1], list_NRU[i][2], pro[0], pro[1]]
-      	if data not in list_nru:
-      		list_nru.append(data)
-        	ccd_nru += list_NRU[i][2] 
+	#==================== Get product ID ===================
+	statement = 'Select PRODUCT_ID, CCD_PRODUCT from ODS_META_PRODUCT where PRODUCT_ID = :1'
+	cursor.execute(statement, (product))
+	list_product = list(cursor.fetchall())
 
-  return ccd_nru 
+	ccd_nru = 0  
+	list_nru = []
+	for i in range(len(list_NRU)):
+		list_NRU[i] = list(list_NRU[i])    
+		for pro in list_product:
+			if (list_NRU[i][1] == pro[1]):
+				data = [list_NRU[i][0], list_NRU[i][1], list_NRU[i][2], pro[0], pro[1]]
+				if data not in list_nru:
+					list_nru.append(data)
+					ccd_nru += list_NRU[i][2] 
+
+	return ccd_nru 
         
 
 
 
 def add_NRU_monthly_for plan(connect, path_folder, list_plan):
-  # ==================== Connect database =======================
-  conn = cx_Oracle.connect(connect)
-  cursor = conn.cursor()
+# ==================== Connect database =======================
+	conn = cx_Oracle.connect(connect)
+	cursor = conn.cursor()
 
-  for plan in list_plan:
-    if ('MONTHLY' in value):
-      for i in range(len(value['MONTHLY'])):
-        plan['MONTHLY']['CCD_NRU'] = Read_NRU_for_month(cursor, str(value['MONTHLY'][i]['MONTH']), value['PRODUCT'])
+	for plan in list_plan:
+		if ('MONTHLY' in value):
+			for i in range(len(value['MONTHLY'])):
+				plan['MONTHLY']['CCD_NRU'] = Read_NRU_for_month(cursor, str(value['MONTHLY'][i]['MONTH']), value['PRODUCT'])
 
-  cursor.close()
-  return list_plan
+	cursor.close()
+	return list_plan
 
 
 
