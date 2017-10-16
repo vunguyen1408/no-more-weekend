9c9e47108d9a492321d74a74b7e4f58c60619a96
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-16 22:35:35 +0700

    Update mapping WPL

--- adwords_python3/online_marketing/final/mapping_campaign_plan.py
+++ adwords_python3/online_marketing/final/mapping_campaign_plan.py
@@ -165,6 +165,9 @@ def MapAccountWithCampaignWPL(path_folder, list_plan, list_campaign, date):
 
       if str(eform['REASON_CODE_ORACLE']) == '1708039' and int(eform['PRODUCT']) == 160:
         print (eform['CCD_PRODUCT'])
+        print (checkProductCode(camp['Account Name'], eform['CCD_PRODUCT']))
+        print (camp['Account Name'])
+        print ("===============================================")
 
       if (camp['Mapping'] == False and eform['DEPARTMENT_NAME'] == 'WPL'): 
         if (  (eform['PRODUCT_CODE'] != []) and checkProductCode(camp['Account Name'], eform['CCD_PRODUCT']) and \
