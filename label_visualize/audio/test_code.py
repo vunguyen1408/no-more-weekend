c1a1ec23368a03629239f5d7682b8fae776660da
thuydungtk <thuydung.cobala@gmail.com>
2017-10-16 13:52:37 +0700

    Update audio

--- label_visualize/audio/test_code.py
+++ label_visualize/audio/test_code.py
@@ -7,14 +7,14 @@ from datetime import datetime , timedelta, date
 
 file_video = '/home/marketingtool/Workspace/Python/no-more-weekend/label_visualize/audio/2016-10-01_98.mp4'
 file_audio = '/home/marketingtool/Workspace/Python/no-more-weekend/label_visualize/audio/2016-10-01_98.flac'
-print('11111111111111111111111111111')
-subprocess.call(["ffmpeg", "-i", file_video,"-c:a", "flac", file_audio])
-print()
+# print('11111111111111111111111111111')
+# subprocess.call(["ffmpeg", "-i", file_video,"-c:a", "flac", file_audio])
+# print()
 
-print('22222222222222222222222222222')
-print(file_video + '.16.flac')
-subprocess.call(["sox", file_audio, "--channels=1", "--bits=16", file_audio[0:-5] + '.16.flac'])
-print()
+# print('22222222222222222222222222222')
+# print(file_video + '.16.flac')
+# subprocess.call(["sox", file_audio, "--channels=1", "--bits=16", file_audio[0:-5] + '.16.flac'])
+# print()
 
 print('33333333333333333333333333333')
 subprocess.call(["ffprobe", "-i", file_audio[0:-5] + '.16.flac', "-show_streams |", "grep", "sample_rate"])
