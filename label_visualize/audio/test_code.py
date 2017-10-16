612a3c7b325f4291e9d8a7d04f19f59748404be2
thuydungtk <thuydung.cobala@gmail.com>
2017-10-16 16:28:10 +0700

    Update audio

--- label_visualize/audio/test_code.py
+++ label_visualize/audio/test_code.py
@@ -18,13 +18,14 @@ file_audio = '/home/marketingtool/Workspace/Python/no-more-weekend/label_visuali
 
 print('33333333333333333333333333333')
 print(file_audio[0:-5] + '.16.flac')
-out = subprocess.call(["ffprobe", file_audio[0:-5] + '.16.flac', "-show_streams| grep", "stream=sample_rate"]) 
+# out = subprocess.call(["ffprobe", file_audio[0:-5] + '.16.flac', "-show_streams| grep", "stream=sample_rate"]) 
 # cmd = "ffprobe " + file_audio[0:-5] + '.16.flac'
 # out = subprocess.Popen(["ffprobe", file_audio[0:-5] + '.16.flac'])
 # stdout, stderr = out.communicate()
 # print("====================================")
 # a = out.stdout.close()
 # out = subprocess.getoutput(cmd)
+out = subprocess.call(["ffprobe", file_audio[0:-5] + '.16.flac', "-show_streams", "stream=sample_rate"]) 
 
 print(out)
 print(type(out))
