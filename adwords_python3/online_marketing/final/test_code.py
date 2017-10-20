6170d6e5d3d1392bfd80970e65c22990d66bf561
thuydungtk <thuydung.cobala@gmail.com>
2017-10-20 10:59:21 +0700

    Update

--- adwords_python3/online_marketing/final/test_code.py
+++ adwords_python3/online_marketing/final/test_code.py
@@ -17,9 +17,9 @@ def Insert(name, cursor):
 		
 	cursor.execute(statement, (name, None))
 	
-	# print("A row inserted!.......")
+	print("A row inserted!.......")
 	conn.commit()
-	# print("Committed!.......")
+	print("Committed!.......")
 
 
 
@@ -37,8 +37,11 @@ input_file  = codecs.open(path, "r", encoding="utf-8")
 data = json.loads(input_file.read())
 for acc in data:
 	if (str(acc["customerId"]) == '4476024314'):
+		# Insert(acc["name"].encode('utf-8'), cursor)
+		# print(acc["name"])
+		# print(acc["name"].encode('utf-8'))
 		sys.stdout = codecs.getwriter("iso-8859-1")(sys.stdout, 'xmlcharrefreplace')		
-		Insert(acc["name"].encode('utf-8'), cursor)
+		Insert(acc["name"], cursor)
 		# Insert(acc["name"].encode('utf-8-sig'), cursor)
 		# Insert(acc["name"], cursor)
 		# Insert(acc["name"].encode('iso-8859-1'), cursor)
