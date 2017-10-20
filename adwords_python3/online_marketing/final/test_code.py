86484598de6096062947bf5435d7f166273e78f0
thuydungtk <thuydung.cobala@gmail.com>
2017-10-20 10:50:09 +0700

    Update

--- adwords_python3/online_marketing/final/test_code.py
+++ adwords_python3/online_marketing/final/test_code.py
@@ -17,7 +17,7 @@ def Insert(name, cursor):
 		
 	cursor.execute(statement, (name, None))
 	
-	print("A row inserted!.......")
+	# print("A row inserted!.......")
 	conn.commit()
 	# print("Committed!.......")
 
