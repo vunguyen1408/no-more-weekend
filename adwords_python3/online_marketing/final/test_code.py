2156a351ceffd75927270dd1a6f8409949e0cd77
thuydungtk <thuydung.cobala@gmail.com>
2017-10-20 10:02:20 +0700

    Update GG

--- adwords_python3/online_marketing/final/test_code.py
+++ adwords_python3/online_marketing/final/test_code.py
@@ -356,9 +356,26 @@ cursor = conn.cursor()
 path = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/final/LIST_ACCOUNT/TEST_UNICODE.json'
 
 # data = json.load(codecs.open(path, 'r', 'utf-8-sig'))
-data = json.loads(open(path).read().decode('utf-8-sig'))
+# data = json.loads(open(path).read().decode('utf-8-sig'))
+# for acc in data:
+# 	if (str(acc["customerId"]) == '4476024314'):
+# 		print(acc["name"])
+# 		print(acc["name"].encode('utf-8'))
+		# Insert(acc["name"].encode('utf-8'), cursor)
+
+
+
+
+# import json
+import sys
+# import codecs
+# path = 'TEST_UNICODE.json'
+input_file  = file("TEST_UNICODE.json", "r")
+data = json.loads(input_file.read().decode("utf-8-sig"))
+# output_file = codecs.open("output_file.json", "w", encoding="utf-8")
 for acc in data:
-	if (str(acc["customerId"]) == '4476024314'):
-		print(acc["name"])
-		print(acc["name"].encode('utf-8'))
-		Insert(acc["name"].encode('utf-8'), cursor)
+    if (str(acc["customerId"]) == '4476024314'):
+        sys.stdout = codecs.getwriter("iso-8859-1")(sys.stdout, 'xmlcharrefreplace')
+        print( acc["name"])
+        Insert(acc["name"], cursor)
+        # json.dump(acc["name"], output_file, indent=4, sort_keys=True, ensure_ascii=False)
