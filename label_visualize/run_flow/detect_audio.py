189042c108550b93856ecdcead1b11bf25f29aa1
japonikaa@gmail.com <japonikaa@gmail.com>
2017-10-20 11:34:07 +0700

    leth

--- label_visualize/run_flow/detect_audio.py
+++ label_visualize/run_flow/detect_audio.py
@@ -101,8 +101,9 @@ def get_label_videos(folder, path_folder_audios, video_json):
             value['audio_text']['transcript'] = ''
             value['audio_text']['confidence'] = 0
 
-        print (value['audio_text'])
-        print(value['audio_text']['transcript'])
+        if 'transcript' in value['audio_text'] :
+            print ("Found")
+        #print(value['audio_text']['transcript'])
 
         #if not (value['audio_text']['transcript'] != ""):
         if value['audio_text']['transcript'] == '' :
