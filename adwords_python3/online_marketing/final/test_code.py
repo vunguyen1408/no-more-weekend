e4a46daadb84fb5e2b7a8948f8e7a1eb4c888662
thuydungtk <thuydung.cobala@gmail.com>
2017-10-20 11:12:48 +0700

    update

--- adwords_python3/online_marketing/final/test_code.py
+++ adwords_python3/online_marketing/final/test_code.py
@@ -42,6 +42,7 @@ for acc in data:
 		# print(acc["name"].encode('utf-8'))
 		# sys.stdout = codecs.getwriter("iso-8859-1")(sys.stdout, 'xmlcharrefreplace')	
 		# Insert(acc["name"].encode('utf-8'), cursor)	
+		print(str(acc["name"].encode('utf-8'))
 		Insert(str(acc["name"].encode('utf-8')), cursor)
 		# Insert(acc["name"].encode('utf-8-sig'), cursor)
 		# Insert(acc["name"], cursor)
