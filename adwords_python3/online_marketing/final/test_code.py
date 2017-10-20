37a01fc9a286f1e12d83310319a81505f322eb53
thuydungtk <thuydung.cobala@gmail.com>
2017-10-20 11:24:28 +0700

    Update GG

--- adwords_python3/online_marketing/final/test_code.py
+++ adwords_python3/online_marketing/final/test_code.py
@@ -13,9 +13,9 @@ import time
 def Insert(name, cursor):
 	#==================== Insert data into database =============================
 	statement = 'insert into DTM_GG_RUN_FLAG (FLAG_RUNNING, FINAL_RUNTIME) \
-	values (TO_NCHAR("' + name + '"), '') '
+	values (:1, :2) '
 		
-	cursor.execute(statement)
+	cursor.executemany(statement, (name, None))
 	
 	# print("A row inserted!.......")
 	conn.commit()
@@ -23,9 +23,9 @@ def Insert(name, cursor):
 
 
 
-connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
-conn = cx_Oracle.connect(connect)
-cursor = conn.cursor()
+# connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
+# conn = cx_Oracle.connect(connect)
+# cursor = conn.cursor()
 path = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/final/LIST_ACCOUNT/output_file.json'
 
 # data = json.load(codecs.open(path, 'r', 'utf-8-sig'))
@@ -37,17 +37,13 @@ input_file  = codecs.open(path, "r", encoding="utf-8")
 data = json.loads(input_file.read())
 for acc in data:
 	if (str(acc["customerId"]) == '4476024314'):
-		# Insert(acc["name"].encode('utf-8'), cursor)
-		print(acc["name"])
-		# print(acc["name"].encode('utf-8'))
+		# print(acc["name"])
+		print(acc["name"].encode('utf8').strip())
 		# sys.stdout = codecs.getwriter("iso-8859-1")(sys.stdout, 'xmlcharrefreplace')	
-		# Insert(acc["name"].encode('utf-8'), cursor)	
-		print(str(acc["name"].encode('utf-8')))
-		Insert(str(acc["name"].encode('utf-8')), cursor)
-		# Insert(acc["name"].encode('utf-8-sig'), cursor)
-		# Insert(acc["name"], cursor)
-		# Insert(acc["name"].encode('iso-8859-1'), cursor)
-
+		Insert(acc["name"].encode('utf8').strip(), cursor)	
+		# print(str(acc["name"].encode('utf-8')))
+		# Insert(str(acc["name"].encode('utf-8')), cursor)
+		
 
 
 
