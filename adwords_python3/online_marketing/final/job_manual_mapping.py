7b26fd7417811546284a9a8216ee67f9cbf153ab
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-25 10:11:13 +0700

    Update

--- adwords_python3/online_marketing/final/job_manual_mapping.py
+++ adwords_python3/online_marketing/final/job_manual_mapping.py
@@ -49,6 +49,6 @@ def ManualMapping (connect, path_data, date):
 
 date = '2017-08-31'
 # path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA'
-path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/TEMP_DATA'
+path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA'
 connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
 ManualMapping (connect, path_data, date)
