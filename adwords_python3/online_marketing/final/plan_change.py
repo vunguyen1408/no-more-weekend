751333f489894ac8d79157090471c52dc4cbfe49
thuydungtk <thuydung.cobala@gmail.com>
2017-10-25 12:38:04 +0700

    Update

--- adwords_python3/online_marketing/final/plan_change.py
+++ adwords_python3/online_marketing/final/plan_change.py
@@ -38,10 +38,10 @@ def GetListPlanChange(connect, path_data, date):
 	list_update = []
 	
 
+	#=========== Check list plan change or new plan ================
 	print(len(list_plan_diff))
 	for plan in list_modified_plan:		
-		for value in data['plan']:	
-			# if (value['REAL_START_DATE'] is not None) or (value['REAL_END_DATE'] is not None):
+		for value in data['plan']:				
 			if (plan[6] == value['REASON_CODE_ORACLE']) and (plan[5] == value['PRODUCT']) and \
 			(plan[11] == value['FORM_TYPE']) and (plan[12] == value['UNIT_OPTION']) and \
 			(((plan[8].strftime('%Y-%m-%d') == value['START_DAY']) and (plan[9].strftime('%Y-%m-%d') == value['END_DAY_ESTIMATE'])) or \
@@ -50,69 +50,15 @@ def GetListPlanChange(connect, path_data, date):
 					print("--------------", plan)
 					list_plan_diff.remove(plan)	
 
-			# if (value['REAL_START_DATE'] is None) and (value['REAL_END_DATE'] is None):				
-			# 	if (plan[6] == value['REASON_CODE_ORACLE']) and (plan[5] == value['PRODUCT']) and \
-			# 	(plan[11] == value['FORM_TYPE']) and (plan[12] == value['UNIT_OPTION']) and \
-			# 	(plan[8].strftime('%Y-%m-%d') == value['START_DAY']) and (plan[9].strftime('%Y-%m-%d') == value['END_DAY_ESTIMATE']):					
-			# 		if (plan in list_plan_diff):
-			# 			print("++++++++++++++++++++", plan)
-			# 			list_plan_diff.remove(plan)
 
-			
-
-
-
-	# print(type(list_modified_plan[0][6]), type(data['plan'][0]['REASON_CODE_ORACLE']))
-	# print(type(list_modified_plan[0][5]), type(data['plan'][0]['PRODUCT']))
-	# print(type(list_modified_plan[0][11]), type(data['plan'][0]['FORM_TYPE']))
-	# print(type(list_modified_plan[0][12]), type(data['plan'][0]['UNIT_OPTION']))
-	# print(type(list_modified_plan[0][8]), type(datetime.strptime(data['plan'][0]['START_DAY'], '%Y-%m-%d')))
-	# print(type(list_modified_plan[0][9]), type(datetime.strptime(data['plan'][0]['END_DAY_ESTIMATE'], '%Y-%m-%d')))
-	# print(type(list_modified_plan[0][23]), type(datetime.strptime(data['plan'][0]['REAL_START_DATE'], '%Y-%m-%d')))
-	# print(type(list_modified_plan[0][24]), type(datetime.strptime(data['plan'][0]['REAL_END_DATE'], '%Y-%m-%d')))
-	
-
-
-
-
+	#========== Update new plan for file plan ===============	
 	
 	
-	
-
-			
-
-
-	# for plan in list_modified_plan:
-	# 	check = False
-	# 	for value in data['plan']:
-	# 		#================ Not change plan code ================
-	# 		if (plan[6] == value['REASON_CODE_ORACLE']):
-	# 			flag = True
-	# 			#=========== Change Real start date, Read end date ===========
-
-	# 			# if (plan[8] != value['START_DAY']) and (plan[9] != value['END_DAY_ESTIMATE']):
-	# 			# 	list_change_date.append(plan)
-	# 			# 	list_plan_diff.append(plan)
-	# 			# elif (plan[6] != value['PRODUCT']) or (plan[11] != value['FORM_TYPE']) or (plan[12] != value['UNIT_OPTION']):
-	# 			# 	list_change.append(plan)
-	# 			# 	list_plan_diff.append(plan)
-	# 			# elif (plan[1] != value['CMONTH']) or (plan[3] != value['DEPARTMENT']) or (plan[7] != value['EFORM_NO']) \
-	# 			# 	or  (plan[13] != value['UNIT_COST']) or (plan[14] != value['AMOUNT_USD']):
-	# 			# 	list_update.append(plan)
-	# 			if (plan[5] == value['PRODUCT']) and (plan[11] == value['FORM_TYPE']) and (plan[12] == value['UNIT_OPTION']) \
-	# 			and (plan[8] == value['START_DAY']) and (plan[9] == value['END_DAY_ESTIMATE']) :
-	# 				check = True
-	# 	if (check == False):
-	# 		list_plan_diff.append(plan)
-	# 	if (flag == False):
-	# 		list_change_plancode.append(plan)
-	# 		list_plan_diff.append(plan)
-
-	
-	for plan in list_plan_diff:
-		print(plan)
-	print(len(list_plan_diff))
+	print('list_plan_diff: ', len(list_plan_diff))
 	return list_plan_diff
+
+
+
 
 
 connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
