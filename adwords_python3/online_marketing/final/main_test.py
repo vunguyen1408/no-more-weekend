9fea84d38db0400f775d3228bdcb796e808ee913
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-25 09:10:37 +0700

    Update

--- adwords_python3/online_marketing/final/main_test.py
+++ adwords_python3/online_marketing/final/main_test.py
@@ -84,17 +84,17 @@ def Daily(connect, path_data, date):
 	# print ("\n\n======================= RUN INSERT INSTALL WITH DATE : " + date + " =========================")
 	# insert_install = time.time()
 
-	install.RunInsertInstall(connect, path_data, list_customer_id, date)
+	# install.RunInsertInstall(connect, path_data, list_customer_id, date)
 
 	# # time_insert = time.time() - insert_install
 
 
 
 	# #------------------ Read log manual mapping and get plan NRU ---------------------
-	mapping_data.ReadPlanFromTable(connect, path_data, date)
+	# mapping_data.ReadPlanFromTable(connect, path_data, date)
 	
-	mapping_data.ReadProductAlias(connect, path_data, date)
-	manual.ReadTableManualMap(connect, path_data, date)
+	# mapping_data.ReadProductAlias(connect, path_data, date)
+	# manual.ReadTableManualMap(connect, path_data, date)
 	# #----------------------------------------------------------------
 	# # print ("             Time insert install: ", time_insert)
 
@@ -103,7 +103,7 @@ def Daily(connect, path_data, date):
 	# #======================== Mapping data for list account ============================
 	# # print ("\n\n======================= RUN MAPPING WITH DATE : " + date + " =========================")
 	# # mapping = time.time()
-	mapping_data.MapDataForAllAccount(list_customer_id, path_data, date)
+	# mapping_data.MapDataForAllAccount(list_customer_id, path_data, date)
 	# # time_mapping = time.time() - mapping
 	# # print ("             Time maping: ", time_mapping)
 
@@ -112,7 +112,7 @@ def Daily(connect, path_data, date):
 	# #============================== Merge data ===============================
 	# # print ("\n\n======================= RUN MERGE WITH DATE : " + date + " =========================")
 	# # merge = time.time()
-	merge_date.Merge(path_data, list_customer_id, date)
+	# merge_date.Merge(path_data, list_customer_id, date)
 	# # time_merge = time.time() - merge
 	# # print ("             Time merge: ", time_merge)
 
@@ -120,7 +120,7 @@ def Daily(connect, path_data, date):
 	# #============================== Insert data mapping to total ===============================
 	# # print ("\n\n============= RUN INSERT DATA MAPPING TO TOTAL WITH DATE : " + date + " =================")
 	# # insert_total = time.time()
-	insert_to_total.InsertDateToTotal(path_data, date)
+	# insert_to_total.InsertDateToTotal(path_data, date)
 	# # time_insert_total = time.time() - insert_total
 	# # print ("            Time insert data mapping to total : ", time_insert_total)
 
@@ -135,7 +135,7 @@ def Daily(connect, path_data, date):
 	# #======================== Insert nru to plan ==============================
 	# # print ("\n\n======================= RUN INSERT NRU WITH DATE : " + date + " =========================")
 	# # insert_nru = time.time()
-	# nru.Add_Data_To_Plan(connect, path_data, date)
+	nru.Add_Data_To_Plan(connect, path_data, date)
 	# # time_insert = time.time() - insert_install
 
 	#======================== History name ==================================
@@ -150,10 +150,10 @@ def Daily(connect, path_data, date):
 	list_map = []
 	list_camp_remove = []
 	
-	# monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# detail_map.InsertDataMapToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	detail_map.InsertDataMapToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
 
 
 	# time_insert_databse = time.time() - insert_databse
