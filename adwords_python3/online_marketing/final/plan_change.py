b90f2a4ac7a58d8b8aa1ebcdd7dd46cc90b1216f
thuydungtk <thuydung.cobala@gmail.com>
2017-10-25 13:54:51 +0700

    Update

--- adwords_python3/online_marketing/final/plan_change.py
+++ adwords_python3/online_marketing/final/plan_change.py
@@ -2,6 +2,8 @@ import json
 import os
 import cx_Oracle 
 from datetime import datetime , timedelta, date
+import manual_mapping_and_remap as manual
+import mapping_campaign_plan as mapping
 
 
 def ReadPlanFromTable(connect):
@@ -22,7 +24,6 @@ def ReadPlanFromTable(connect):
 	for i in range(len(list_modified_plan)):
 		list_modified_plan[i] = list(list_modified_plan[i])
 	return list_modified_plan
-
 
 
 def GetListPlanChange(connect, path_data, date):
@@ -52,10 +53,89 @@ def GetListPlanChange(connect, path_data, date):
 
 
 	#========== Update new plan for file plan ===============	
+	mapping.ReadPlanFromTable(connect, path_data, str(date))
+	mapping.ReadProductAlias(connect, path_data, str(date))	
+	#========================================================
 	
-	
+	for plan in list_plan_diff:
+		print(plan)
 	print('list_plan_diff: ', len(list_plan_diff))
+	
 	return list_plan_diff
+
+
+
+# def AutoMap(connect, path_data, date):
+# 	# ------------- Get new plan or change plan ----------------	
+# 	list_plan = GetListPlanChange(connect, path_data, date)
+# 	print (len(list_plan))
+# 	# if len(list_plan) > 0:
+# 	# ------------- Get campaign for mapping ----------------	
+# 	path_data_total_map = os.path.join(path_data + '/' + str(date) + '/DATA_MAPPING', 'total_mapping' + '.json')
+	
+# 	list_map_all = []
+# 	list_plan_remove_unmap = []
+# 	list_camp_remove_unmap = []
+# 	list_plan_update = []
+# 	if not os.path.exists(path_data_total_map):
+# 		i = 0
+# 		find = True
+# 		date_before = datetime.strptime(date, '%Y-%m-%d').date() - timedelta(1)
+# 		path_data_total_map = os.path.join(path_data + '/' + str(date_before) + '/DATA_MAPPING', 'total_mapping' + '.json')
+# 		while not os.path.exists(path_data_total_map):
+# 			i = i + 1
+# 			date_before = date_before - timedelta(1)
+# 			path_data_total_map = os.path.join(path_data + '/' + str(date_before) + '/DATA_MAPPING', 'total_mapping' + '.json')
+# 			if i == 60:
+# 				find = False
+# 				break			
+# 	else:
+# 		find = True
+
+# 	if find:
+# 		with open (path_data_total_map,'r') as f:
+# 			data_total = json.load(f)
+
+# 	list_full_camp = data_total['UN_CAMPAIGN']
+# 	list_camp_all = []
+# 	list_camp_GS5 = []
+# 	list_camp_WPL = []
+# 	for camp in list_full_camp:
+# 		if (camp['Dept'] == 'GS5'):
+# 			list_camp_GS5.append(camp)
+# 		elif (camp['Dept'] == 'WPL'):
+# 			list_camp_GS5.append(camp)
+# 		else:
+# 			list_camp_all.append(camp)
+
+# 	print(len(list_full_camp))
+# 	print(len(list_camp_all))
+# 	print(len(list_camp_GS5))
+# 	print(len(list_camp_WPL))
+
+				
+		# list_camp_remove_unmap = []
+		# list_map_all = []
+		# list_plan_remove_unmap = []
+		# # print (len(data_total['UN_CAMPAIGN']))
+		# for plan in list_plan:
+		# 	plan, list_map, list_camp_need_remove = GetCampaignUnMapForPlan(plan, path_data_total_map)
+		# 	# print ("--------------- gggg---------------")
+		# 	# print (plan)
+		# 	# print (list_camp_need_remove)
+		# 	# print ("---------------gggg ---------------")
+		# 	#------------- Insert data map ------------
+		# 	data_total['MAP'].extend(list_map)
+		# 	# print (len(list_camp_need_remove))
+
+		# 	#----------- Remove unmap ---------------------
+		# 	for camp in list_camp_need_remove:
+		# 		for campaign in data_total['UN_CAMPAIGN']:
+		# 			if camp['Campaign ID'] == campaign['Campaign ID'] \
+		# 				and camp['Date'] == campaign['Date']:
+		# 				data_total['UN_CAMPAIGN'].remove(campaign)
+		# 				list_camp_remove_unmap.append(campaign)
+
 
 
 
