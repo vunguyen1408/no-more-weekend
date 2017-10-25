713462a061d40dfe5dcf665b5f8bcbaf7c208811
thuydungtk <thuydung.cobala@gmail.com>
2017-10-25 10:52:11 +0700

    update

--- adwords_python3/online_marketing/final/get_data/get_accounts.py
+++ adwords_python3/online_marketing/final/get_data/get_accounts.py
@@ -147,10 +147,11 @@ path_config = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_pyth
 file_json = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/final/LIST_ACCOUNT/MCC_111.json'
 # path_config = 'D:/WorkSpace/GG_Tool/Finally/no-more-weekend/adwords_python3/online_marketing/final/googleads_MCC.yaml'
 # file_json = 'D:/WorkSpace/Adwords/Finanlly/AdWords/FULL_DATA/MCC_entity.json'
+print("Start ........")
 root_note, list_acc = GetAllAcount(path_config)
 with open(file_json, 'w') as fo:
   json.dump(root_note[1], fo)
-  
+
 
 # path_config = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/final/googleads_WPL.yaml'
 # file_json = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/final/LIST_ACCOUNT/WPL.json'
