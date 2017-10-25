6547788239b01f9bdd2f2e05306e564239386ec6
thuydungtk <thuydung.cobala@gmail.com>
2017-10-25 10:38:08 +0700

    Update

--- adwords_python3/online_marketing/final/plan_change.py
+++ adwords_python3/online_marketing/final/plan_change.py
@@ -63,8 +63,8 @@ def GetListPlanChange(connect, path_data, date):
 	print(type(list_modified_plan[0][12]), type(data['plan'][0]['UNIT_OPTION']))
 	print(type(list_modified_plan[0][8]), type(datetime.strptime(data['plan'][0]['START_DAY'], '%Y-%m-%d')))
 	print(type(list_modified_plan[0][9]), type(datetime.strptime(data['plan'][0]['END_DAY_ESTIMATE'], '%Y-%m-%d')))
-	print(type(list_modified_plan[0][23]), type(datetime.strptime(data['plan'][0]['REAL_START_DATE'], '%Y-%m-%d')))
-	print(type(list_modified_plan[0][24]), type(datetime.strptime(data['plan'][0]['REAL_END_DATE'], '%Y-%m-%d')))
+	# print(type(list_modified_plan[0][23]), type(datetime.strptime(data['plan'][0]['REAL_START_DATE'], '%Y-%m-%d')))
+	# print(type(list_modified_plan[0][24]), type(datetime.strptime(data['plan'][0]['REAL_END_DATE'], '%Y-%m-%d')))
 	
 
 
