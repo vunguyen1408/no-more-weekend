243e11e35f1b840679a8103eaa8021ccbed7a7db
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-26 13:19:43 +0700

    Update

--- adwords_python3/online_marketing/final/main_run.py
+++ adwords_python3/online_marketing/final/main_run.py
@@ -134,13 +134,14 @@ def Daily(connect, path_data, date):
 
 	
 	#======================== Insert nru to plan ==============================
-	# print ("\n\n======================= RUN INSERT NRU WITH DATE : " + date + " =========================")
+	print ("\n\n======================= RUN INSERT NRU WITH DATE : " + date + " =========================")
 	# insert_nru = time.time()
 	nru.Add_Data_To_Plan(connect, path_data, date)
 	# time_insert = time.time() - insert_install
 
 
 	# ======================= Insert branding install ====================================
+	print ("\n\n======================= Insert branding install : " + date + " =========================")
 	insert_install_brandingGPS.AddBrandingGPSToPlan(path_data, connect, date)
 
 	#======================== History name ==================================
