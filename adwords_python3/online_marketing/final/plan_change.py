6d9c8674dbf805147e577d3bab8422eb075d5479
thuydungtk <thuydung.cobala@gmail.com>
2017-10-26 11:17:16 +0700

    update

--- adwords_python3/online_marketing/final/plan_change.py
+++ adwords_python3/online_marketing/final/plan_change.py
@@ -240,7 +240,9 @@ def AutoMap(connect, path_data, date):
 			list_camp_all = []
 			list_camp_GS5 = []
 			list_camp_WPL = []
-			for camp in list_full_camp:		
+			for camp in list_full_camp:	
+				if 	camp['Advertising Channel'].find('Search') >= 0:
+					print(camp['Campaign ID'])
 				if (mapping.CheckIsAccountGS5(path_data, camp['Account ID'])):
 				# if (camp['Dept'] == 'GS5'):
 					list_camp_GS5.append(camp)
