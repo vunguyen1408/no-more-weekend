32bed2754a664ddcb3cdece153479bdd06cf5fc6
ThanhDuon2712 <ltduong1994@gmail.com>
2017-10-27 00:54:08 +0700

    Update

--- adwords_python3/online_marketing/final/main_run.py
+++ adwords_python3/online_marketing/final/main_run.py
@@ -134,11 +134,11 @@ def Daily(connect, path_data, date):
 
 	
 	#======================== Insert nru to plan ==============================
-	print ("\n\n======================= RUN INSERT NRU WITH DATE : " + date + " =========================")
-	insert_nru = time.time()
-	nru.Add_Data_To_Plan(connect, path_data, date)
-	time_insert = time.time() - insert_nru
-	print ("---------- Time insert nru to total : ", time_insert)
+	# print ("\n\n======================= RUN INSERT NRU WITH DATE : " + date + " =========================")
+	# insert_nru = time.time()
+	# nru.Add_Data_To_Plan(connect, path_data, date)
+	# time_insert = time.time() - insert_nru
+	# print ("---------- Time insert nru to total : ", time_insert)
 
 
 	# ======================= Insert branding install ====================================
@@ -158,10 +158,10 @@ def Daily(connect, path_data, date):
 	list_map = []
 	list_camp_remove = []
 	
-	# monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
-	# detail_map.InsertDataMapToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
+	detail_map.InsertDataMapToDatabase(path_data, connect, list_map, list_plan_remove, list_camp_remove, date)
 
 
 	# time_insert_databse = time.time() - insert_databse
