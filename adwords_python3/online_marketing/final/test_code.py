1dd56d3b22c2f73425eeef125ed947a4d6fe126b
thuydungtk <thuydung.cobala@gmail.com>
2017-10-27 14:01:10 +0700

    update

--- adwords_python3/online_marketing/final/test_code.py
+++ adwords_python3/online_marketing/final/test_code.py
@@ -478,9 +478,14 @@ path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/TEST_DATA'
 date = '2017-05-31' 
 conn = cx_Oracle.connect(connect, encoding = "UTF-8", nencoding = "UTF-8")
 cursor = conn.cursor()
-final_log = datetime.now() - datetime(2017, 10, 27, 00, 00, 00)
+final_log = datetime.now() 
 print(final_log)
-list_plan_diff = GetListPlanChangeFromTable(cursor, final_log)
+
+path_log = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/final/LIST_ACCOUNT/log_plan_change.txt'
+fi = open(path_log, 'a+') 
+fi.writelines(final_log)
+print("Save log ok..........")
+# list_plan_diff = GetListPlanChangeFromTable(cursor, final_log)
 # list_plan_diff = GetListPlanChange(connect, path_data, date)
 # list_data_map, list_plan_remove_unmap, list_camp_remove_unmap, list_plan_update, list_plan_insert = AutoMap(connect, path_data, date)
 
