65fd5175965c714583c04b268382e79259af29d5
duonglt <ltduong1994@gmail.com>
2017-10-30 14:15:00 +0700

    Update

--- adwords_python3/online_marketing/final/main.py
+++ adwords_python3/online_marketing/final/main.py
@@ -184,18 +184,18 @@ def ManyDate(connect, path_data, start_date, end_date):
 	# adwords_client = adwords.AdWordsClient.LoadFromStorage()
 	path_acc = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/final/LIST_ACCOUNT'
 	list_acc, list_mcc, list_dept = add_acc_name.get_list_customer(path_acc)
-	print (len(list_acc))
+	# print (len(list_acc))
 
-	# date_ = datetime.strptime(start_date, '%Y-%m-%d').date()
-	# to_date_ = datetime.strptime(end_date, '%Y-%m-%d').date()
-	# n = int((to_date_ - date_).days)
-	# s = time.time()
-	# for i in range(n + 1):
-	# 	single_date = date_ + timedelta(i)
-	# 	d = single_date.strftime('%Y-%m-%d')
-	# 	Daily(connect, path_data, str(d), list_acc)
-	# e = time.time() - s
-	# print (" TIME : ", e)
+	date_ = datetime.strptime(start_date, '%Y-%m-%d').date()
+	to_date_ = datetime.strptime(end_date, '%Y-%m-%d').date()
+	n = int((to_date_ - date_).days)
+	s = time.time()
+	for i in range(n + 1):
+		single_date = date_ + timedelta(i)
+		d = single_date.strftime('%Y-%m-%d')
+		Daily(connect, path_data, str(d), list_acc)
+	e = time.time() - s
+	print (" TIME : ", e)
 
 
 # start_date = '2017-06-01'
