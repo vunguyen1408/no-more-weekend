2e3c41011ed6f603f9a0ff9e189796f0a98328eb
thuydungtk <thuydung.cobala@gmail.com>
2017-10-31 11:40:46 +0700

    update

--- adwords_python3/online_marketing/final/main_test.py
+++ adwords_python3/online_marketing/final/main_test.py
@@ -105,6 +105,8 @@ def Daily(connect, path_data, date, list_customer_id):
 	nru_test.Add_NRU_into_plan(connect, path_data, date)  
 	mapping_data.ReadProductAlias(connect, path_data, date)	
 	manual.ReadTableManualMap(connect, path_data, date)
+
+	time.sleep(10)
 	#----------------------------------------------------------------
 	# print ("             Time insert install: ", time_insert)
 
