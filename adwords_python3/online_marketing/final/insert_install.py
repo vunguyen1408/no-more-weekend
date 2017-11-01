1355b443c0ff4b9198c3ebedccac81aec0f58558
duonglt <ltduong1994@gmail.com>
2017-11-01 10:54:39 +0700

    Update

--- adwords_python3/online_marketing/final/insert_install.py
+++ adwords_python3/online_marketing/final/insert_install.py
@@ -41,7 +41,7 @@ def GetInstallAppsFlyer(connect, start_date, end_date, media_source, list_produc
 			# print (int(i[3]))
 			list_install_for_product.append(i)
 			# number_install += i[3]
-	print (number_install)
+	# print (number_install)
 	return list_install_for_product
 
 
