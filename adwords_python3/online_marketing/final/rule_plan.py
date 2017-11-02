d78ae068d1b456e5e33c843d0eda1eded8e3bdc6
thuydungtk <thuydung.cobala@gmail.com>
2017-11-02 16:23:05 +0700

    update

--- adwords_python3/online_marketing/final/rule_plan.py
+++ adwords_python3/online_marketing/final/rule_plan.py
@@ -1321,10 +1321,10 @@ date = '2017-09-05'
 path_log = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/final/LIST_ACCOUNT/log_plan_change.txt'
 
 
-# list_camp_remove_unmap, list_camp_insert_unmap, \
-# 	list_plan_insert_total, list_plan_update_total, list_plan_remove_total, \
-# 	list_data_insert_map, list_plan_update_map, list_plan_remove_map, \
-# 	list_plan_insert_unmap, list_plan_remove_unmap = ClassifyPlan(connect, path_data, date, path_log)
+list_camp_remove_unmap, list_camp_insert_unmap, \
+	list_plan_insert_total, list_plan_update_total, list_plan_remove_total, \
+	list_data_insert_map, list_plan_update_map, list_plan_remove_map, \
+	list_plan_insert_unmap, list_plan_remove_unmap = ClassifyPlan(connect, path_data, date, path_log)
 
 
 
