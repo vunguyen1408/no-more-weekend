f54ec56661f1110b75a88dbf09e03718a6ba9bee
duonglt <ltduong1994@gmail.com>
2017-11-04 20:57:46 +0700

    Update

--- adwords_python3/online_marketing/final/a_impove_system/mapping_campaign_plan.py
+++ adwords_python3/online_marketing/final/a_impove_system/mapping_campaign_plan.py
@@ -140,7 +140,7 @@ def MapAccountWithCampaignAll(path_folder, list_plan, list_campaign, date):
   data_map['PLAN'] = list_plan
   print (" -------------- Campaign ------ ", len(list_campaign_map))
   print (" -------------- Mapping------ ", number)
-  print (" -------------- Un mapping------ ", len(list_un_campaign))
+  print (" -------------- Un mapping------ \n", len(list_un_campaign))
   return data_map
 
 #================= Mapping campaign and plan WPL =====================
@@ -198,7 +198,7 @@ def MapAccountWithCampaignWPL(path_folder, list_plan, list_campaign, date):
   data_map['PLAN'] = list_plan
   print (" -------------- Campaign ------ ", len(list_campaign_map))
   print (" -------------- Mapping------ ", number)
-  print (" -------------- Un mapping------ ", len(list_un_campaign))
+  print (" -------------- Un mapping------ \n", len(list_un_campaign))
   return data_map
 
 
@@ -318,7 +318,7 @@ def MapAccountWithCampaignGS5(path_folder, list_plan, list_campaign, date):
   data_map['PLAN'] = list_plan
   print (" -------------- Campaign ------ ", len(list_campaign_map))
   print (" -------------- Mapping------ ", number)
-  print (" -------------- Un mapping------ ", len(list_un_campaign))
+  print (" -------------- Un mapping------ \n", len(list_un_campaign))
   return data_map
 
 
