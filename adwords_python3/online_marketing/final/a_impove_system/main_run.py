4c9b5b0ad09d4666ab7589f9a9f4146dd09e6379
duonglt <ltduong1994@gmail.com>
2017-11-06 06:10:42 +0700

    Update

--- adwords_python3/online_marketing/final/a_impove_system/main_run.py
+++ adwords_python3/online_marketing/final/a_impove_system/main_run.py
@@ -51,23 +51,23 @@ def Daily(connect, path_data, date, list_customer_id):
 	# print ("            Time insert data mapping to total : ", time_insert_total)
 
 	# ======================= Insert branding install ====================================
-	start = time.time()
-	insert_install.InsertInstall(path_data, connect, date)
-	insert_install_brandingGPS.AddBrandingGPS(path_data, connect, date)
-	print ("Time : ", time.time() - start)
+	# start = time.time()
+	# insert_install.InsertInstall(path_data, connect, date)
+	# insert_install_brandingGPS.AddBrandingGPS(path_data, connect, date)
+	# print ("Time : ", time.time() - start)
 
 
-	#=============================== Update to database =========================================
-	# print ("\n\n============= RUN INSERT DATA TO DATABASE WITH DATE : " + date + " =================")
-	# insert_databse = time.time()
-	# list_plan_insert = []
-	# list_plan_update = []
-	# insert_monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
-	# insert_monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
-	# insert_plan_sum.InsertPlanSumToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
-
-	# time_insert_databse = time.time() - insert_databse
-	# print ("            Time insert data to database : ", time_insert_databse)
+	# =============================== Update to database =========================================
+	print ("\n\n============= RUN INSERT DATA TO DATABASE WITH DATE : " + date + " =================")
+	insert_databse = time.time()
+	list_plan_insert = []
+	list_plan_update = []
+	insert_monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
+	insert_monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
+	insert_plan_sum.InsertPlanSumToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
+	insert_data_map.InsertDataMapToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
+	time_insert_databse = time.time() - insert_databse
+	print ("            Time insert data to database : ", time_insert_databse)
 
 
 	
