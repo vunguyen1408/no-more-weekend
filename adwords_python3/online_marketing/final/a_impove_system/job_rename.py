8476f4735e0504d8faea1439cd6a678504aa94c2
duonglt <ltduong1994@gmail.com>
2017-11-06 07:39:33 +0700

    Update

--- adwords_python3/online_marketing/final/a_impove_system/job_rename.py
+++ adwords_python3/online_marketing/final/a_impove_system/job_rename.py
@@ -98,7 +98,7 @@ list_customer_id = [
      ]
 
 
-date = '2017-09-30'
-path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA_GG'
+date = '2017-08-31'
+path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/TEMP_DATA'
 connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
 Rename (connect, path_data, list_customer_id, date)
