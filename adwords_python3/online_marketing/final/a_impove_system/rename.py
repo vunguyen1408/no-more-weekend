14ffd128d533eabe22fd12e8c1d1a326b91da5b2
duonglt <ltduong1994@gmail.com>
2017-11-06 07:54:44 +0700

    Update

--- adwords_python3/online_marketing/final/a_impove_system/rename.py
+++ adwords_python3/online_marketing/final/a_impove_system/rename.py
@@ -218,8 +218,8 @@ def Map(path_folder, list_plan, list_campaign, date):
   #   if (camp['Cost'] > 0) and camp['Campaign state'] != 'Total':
   #     list_campaign_map.append(camp)
 
-  print(len(list_campaign_map))
-  for j, camp in enumerate(list_campaign_map):
+  print(len(list_campaign))
+  for j, camp in enumerate(list_campaign):
     t = False
     camp['Advertising Channel'] = mapping.ChangeCampaignType(camp['Advertising Channel'])
     if 'Plan' not in camp:
