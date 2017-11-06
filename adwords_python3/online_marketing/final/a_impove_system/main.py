0e7389a949ee7c23c228661627aca9b6caa50512
duonglt <ltduong1994@gmail.com>
2017-11-06 10:51:45 +0700

    Update

--- adwords_python3/online_marketing/final/a_impove_system/main.py
+++ adwords_python3/online_marketing/final/a_impove_system/main.py
@@ -62,16 +62,16 @@ def Daily(connect, path_data, date, list_customer_id):
 	history_name.InsertHistoryName(connect, path_data, list_customer_id, date)
 
 	# =============================== Update to database =========================================
-	# print ("\n\n============= RUN INSERT DATA TO DATABASE WITH DATE : " + date + " =================")
-	# insert_databse = time.time()
-	# list_plan_insert = []
-	# list_plan_update = []
-	# insert_monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
-	# insert_monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
-	# insert_plan_sum.InsertPlanSumToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
-	# insert_data_map.InsertDataMapToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
-	# time_insert_databse = time.time() - insert_databse
-	# print ("            Time insert data to database : ", time_insert_databse)
+	print ("\n\n============= RUN INSERT DATA TO DATABASE WITH DATE : " + date + " =================")
+	insert_databse = time.time()
+	list_plan_insert = []
+	list_plan_update = []
+	insert_monthly_detail.InsertMonthlyDetailToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
+	insert_monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
+	insert_plan_sum.InsertPlanSumToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
+	insert_data_map.InsertDataMapToDatabase(path_data, connect, list_plan_insert, list_plan_update, date)
+	time_insert_databse = time.time() - insert_databse
+	print ("            Time insert data to database : ", time_insert_databse)
 
 
 	
