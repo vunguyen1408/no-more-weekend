e1d910a31d584902dbc731b370385a8fc74b0ec2
duonglt <ltduong1994@gmail.com>
2017-11-06 10:23:50 +0700

    Updadte

--- adwords_python3/online_marketing/final/a_impove_system/rename.py
+++ adwords_python3/online_marketing/final/a_impove_system/rename.py
@@ -413,6 +413,7 @@ def CacualatorChange(connect, path_data, list_diff, date):
     import time
 
     # print (list_camp_find[0])
+    list_camp_find = list_camp_find[3:]
     print("MAP")
     start = time.time()
     data_map = Map(path_data, list_plan['plan'], list_camp_find, date)
