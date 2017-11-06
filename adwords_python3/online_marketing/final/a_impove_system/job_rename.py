d0cdb7598b64321403c81c1b9aad1a41fc89d645
duonglt <ltduong1994@gmail.com>
2017-11-07 01:12:25 +0700

    Update

--- adwords_python3/online_marketing/final/a_impove_system/job_rename.py
+++ adwords_python3/online_marketing/final/a_impove_system/job_rename.py
@@ -42,10 +42,10 @@ def Rename (connect, path_data, list_customer_id, date):
 		# monthly_sum.InsertMonthlySumToDatabase(path_data, connect, list_map, list_plan_remove_unmap, list_camp_remove_unmap, date)
 		# plan_sum.InsertPlanSumToDatabase(path_data, connect, list_map, list_plan_remove_unmap, list_camp_remove_unmap, date)
 		# detail_map.InsertDataMapToDatabase(path_data, connect, list_map, list_plan_remove_unmap, list_camp_remove_unmap, date)
-
-		# merge_data_manual_mapping.UpdateRename(connect, list_camp_update, data_total)
-		# merge_data_manual_mapping.merger_data_manual_mapping(connect, list_camp_remove_unmap, list_plan_remove_unmap,\
-  #                                   list_camp_remove_unmap, list_plan_update, list_plan_insert_un_map)
+          is_manual_map = 1
+		merge_data_manual_mapping.UpdateRename(connect, list_camp_update, data_total)
+		merge_data_manual_mapping.merger_data_manual_mapping(connect, list_camp_remove_unmap, list_plan_remove_unmap,\
+                                    list_camp_remove_unmap, list_plan_update, list_plan_insert_un_map, is_manual_map)
 		time_update_manual = time.time() - update_manual
 		print ("---------- Time update rename to total : ", time_update_manual)
 	else:
--- adwords_python3/online_marketing/final/a_impove_system/rename.py
+++ adwords_python3/online_marketing/final/a_impove_system/rename.py
@@ -203,8 +203,8 @@ def CheckNameChange(path_data, list_customer, date):
 
     path_data_his = os.path.join(path_data + '/' + str(date) + '/DATA_MAPPING', 'history_name' + '.json')
     ###########################################
-    # with open (path_data_his,'w') as f:
-    #   json.dump(data_total, f)
+    with open (path_data_his,'w') as f:
+      json.dump(data_total, f)
     ############################################
   print("====================== Length =================")
   return (list_diff, data_total)
@@ -449,13 +449,13 @@ def CacualatorChange(connect, path_data, list_diff, date):
     # print (list_camp_need_remove[0])
 
     ###########################################
-    # path_data_total_map = os.path.join(path_data + '/' + str(date) + '/DATA_MAPPING', 'total_mapping' + '.json')
-    # with open (path_data_total_map,'w') as f:
-    #   json.dump(data_total['TOTAL'], f)
+    path_data_total_map = os.path.join(path_data + '/' + str(date) + '/DATA_MAPPING', 'total_mapping' + '.json')
+    with open (path_data_total_map,'w') as f:
+      json.dump(data_total['TOTAL'], f)
 
-    # path_data_un_map = os.path.join(path_data + '/' + str(date) + '/DATA_MAPPING', 'un_map_camp' + '.json')
-    # with open (path_data_un_map,'w') as f:
-    #   json.dump(data_total['UN_CAMP'], f)
+    path_data_un_map = os.path.join(path_data + '/' + str(date) + '/DATA_MAPPING', 'un_map_camp' + '.json')
+    with open (path_data_un_map,'w') as f:
+      json.dump(data_total['UN_CAMP'], f)
     ##########################################
 
   return (list_plan_remove_unmap, list_camp_need_remove, list_plan_update, list_camp_update)
