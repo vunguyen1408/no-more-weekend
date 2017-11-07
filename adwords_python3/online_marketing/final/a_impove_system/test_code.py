01ab50cf8a482c8c7f43c68775192ddfaf2a93ce
thuydungtk <thuydung.cobala@gmail.com>
2017-11-07 16:36:08 +0700

    update

--- adwords_python3/online_marketing/final/a_impove_system/test_code.py
+++ adwords_python3/online_marketing/final/a_impove_system/test_code.py
@@ -5,8 +5,8 @@ from datetime import datetime , timedelta, date
 # import manual_mapping_and_remap as manual
 import mapping_campaign_plan as mapping
 import insert_data_map_to_total as insert_to_total
-import insert_nru_into_data as nru
-import insert_install_brandingGPS_to_plan as insert_install_brandingGPS
+# import insert_nru_into_data as nru
+# import insert_install_brandingGPS_to_plan as insert_install_brandingGPS
 import insert_install as insert_install
 import time
 
