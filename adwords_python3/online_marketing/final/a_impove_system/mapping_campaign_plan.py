19c3af94db88ea90bdde16d760b00203327f7993
thuydungtk <thuydung.cobala@gmail.com>
2017-11-07 16:37:29 +0700

    update

--- adwords_python3/online_marketing/final/a_impove_system/mapping_campaign_plan.py
+++ adwords_python3/online_marketing/final/a_impove_system/mapping_campaign_plan.py
@@ -100,7 +100,7 @@ def MapAccountWithCampaignAll(path_folder, list_plan, list_campaign, date):
   number = 0
   # Remove line total in report
   for j, camp in enumerate(list_campaign):
-    if (camp['Cost'] > 0) and camp['Campaign state'] != 'Total':
+    if (camp['Cost'] > 0) and ('Campaign state' in camp) and camp['Campaign state'] != 'Total':
       list_campaign_map.append(camp)
 
   for i, eform in enumerate(list_plan):  
@@ -181,7 +181,7 @@ def MapAccountWithCampaignWPL(path_folder, list_plan, list_campaign, date):
   number = 0
   # Remove line total in report
   for j, camp in enumerate(list_campaign):
-    if (camp['Cost'] > 0) and camp['Campaign state'] != 'Total':
+    if (camp['Cost'] > 0) and ('Campaign state' in camp) and camp['Campaign state'] != 'Total':
       list_campaign_map.append(camp)
 
   for i, eform in enumerate(list_plan):  
@@ -276,7 +276,7 @@ def MapAccountWithCampaignGS5(path_folder, list_plan, list_campaign, date):
   number = 0
   # Remove line total in report
   for j, camp in enumerate(list_campaign):
-    if (camp['Cost'] > 0) and camp['Campaign state'] != 'Total':
+    if (camp['Cost'] > 0) and ('Campaign state' in camp) and camp['Campaign state'] != 'Total':
       list_campaign_map.append(camp)
 
 
