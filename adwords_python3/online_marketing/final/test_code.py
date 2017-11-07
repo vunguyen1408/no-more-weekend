7d83cc6678d6ee4ea2af08cac3d187fea869ec00
thuydungtk <thuydung.cobala@gmail.com>
2017-11-07 13:40:20 +0700

    update

--- adwords_python3/online_marketing/final/test_code.py
+++ adwords_python3/online_marketing/final/test_code.py
@@ -1341,7 +1341,7 @@ def ClassifyPlan(connect, path_data, date, path_log):
 
 connect = 'MARKETING_TOOL_01/MARKETING_TOOL_01_9999@10.60.1.42:1521/APEX42DEV'
 path_data = '/u01/app/oracle/oradata/APEX/MARKETING_TOOL_GG/DATA_03_10'
-date = '2017-10-31' 
+date = '2017-04-30' 
 # date = '2017-03-01' 
 path_log = '/home/marketingtool/Workspace/Python/no-more-weekend/adwords_python3/online_marketing/final/LIST_ACCOUNT/log_plan_change.txt'
 
