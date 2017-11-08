ab87b1f151d0d4d6963b9b58755134fa364bebbc
thuydungtk <thuydung.cobala@gmail.com>
2017-11-08 13:34:17 +0700

    update

--- adwords_python3/online_marketing/final/a_impove_system/test_code.py
+++ adwords_python3/online_marketing/final/a_impove_system/test_code.py
@@ -1339,16 +1339,22 @@ def CompareTwoPlan(plan_1, plan_2, list_key):
 	"""
 	check_num = 0
 	for i in range(len(list_key)):
-		if (list_key[i] == 'EFORM_NO'):			
-			if (str(plan_1[list_key[i]]).find(u'\xa0') >= 0):				
-				plan_1[list_key[i]] = plan_1[list_key[i]].replace(u'\xa0', u'')
-			if (str(plan_2[list_key[i]]).find(u'\xa0') >= 0):				
-				plan_2[list_key[i]] = plan_2[list_key[i]].replace(u'\xa0', u'')
+		# if (list_key[i] == 'EFORM_NO'):			
+		if (str(plan_1[list_key[i]]).find(u'\xa0') >= 0):				
+			plan_1[list_key[i]] = plan_1[list_key[i]].replace(u'\xa0', u' ')
+		if (str(plan_2[list_key[i]]).find(u'\xa0') >= 0):				
+			plan_2[list_key[i]] = plan_2[list_key[i]].replace(u'\xa0', u' ')
 
-			if (str(plan_1[list_key[i]]).find(' ') >= 0):
-				plan_1[list_key[i]] = plan_1[list_key[i]].replace(' ', '')
-			if (str(plan_2[list_key[i]]).find(' ') >= 0):
-				plan_2[list_key[i]] = plan_2[list_key[i]].replace(' ', '')
+			# if (str(plan_1[list_key[i]]).find(' ') >= 0):
+			# 	plan_1[list_key[i]] = plan_1[list_key[i]].replace(' ', '')
+			# if (str(plan_2[list_key[i]]).find(' ') >= 0):
+			# 	plan_2[list_key[i]] = plan_2[list_key[i]].replace(' ', '')
+
+		if (isinstance(plan_1[list_key[i]], float)):
+			plan_1[list_key[i]] = round(plan_1[list_key[i]], 0)
+
+		if (isinstance(plan_2[list_key[i]], float)):
+			plan_2[list_key[i]] = round(plan_2[list_key[i]], 0)
 
 		if plan_1[list_key[i]] == plan_2[list_key[i]]:
 			check_num += 1
