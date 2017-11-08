ae0ee36bb8280f266652fdd45c8a4a21ffeae811
duonglt <ltduong1994@gmail.com>
2017-11-08 16:45:21 +0700

    Update

--- label_visualize/run_flow/insert_audit_ads_to_data.py
+++ label_visualize/run_flow/insert_audit_ads_to_data.py
@@ -87,9 +87,10 @@ def InsertContentAds(cursor, ads, d):
 						flag = 0
 					else:
 						flag = 1
+					print (image)
 					cursor.execute(statement, (ads['ad_id'], ads['list_product'][0], image['image_url'], 'image_url', image['percent_predict'], i,  \
 					datetime.strptime(d, '%Y-%m-%d'), datetime.strptime((time.strftime('%Y-%m-%d')), '%Y-%m-%d').date(), flag, image['feature']))
-					print (image)
+					# print (image)
 
 		list_thumbnail = ads['audit_content']['thumbnail_urls']
 		if list_thumbnail != []:
