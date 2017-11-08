6314d8e86b890742f2d62e186aa1fc5fe3c21f51
duonglt <ltduong1994@gmail.com>
2017-11-08 17:08:28 +0700

    Update

--- label_visualize/run_flow/insert_audit_ads_to_data.py
+++ label_visualize/run_flow/insert_audit_ads_to_data.py
@@ -87,9 +87,9 @@ def InsertContentAds(cursor, ads, d):
 						flag = 0
 					else:
 						flag = 1
-					print (flag)
-					print (image)
-					print (str(image['feature']))
+					# print (flag)
+					# print (image)
+					# print (str(image['feature']))
 					cursor.execute(statement, (ads['ad_id'], ads['list_product'][0], image['image_url'], 'image_url', image['percent_predict'], i,  \
 					datetime.strptime(d, '%Y-%m-%d'), datetime.strptime((time.strftime('%Y-%m-%d')), '%Y-%m-%d').date(), flag, str(image['feature'])))
 					# print (image)
