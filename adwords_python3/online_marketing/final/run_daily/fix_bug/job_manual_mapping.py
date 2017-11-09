ec638afd7119f3da0dc6e6b21234e67caa6ff1b7
duonglt <ltduong1994@gmail.com>
2017-11-09 10:30:30 +0700

    Update

--- adwords_python3/online_marketing/final/run_daily/fix_bug/job_manual_mapping.py
+++ adwords_python3/online_marketing/final/run_daily/fix_bug/job_manual_mapping.py
@@ -29,8 +29,8 @@ def ManualMapping (connect, path_data, date):
 		update_manual = time.time()
 		list_plan_insert_un_map = []
 		is_manual_map = 1
-		merge_data_manual_mapping.merger_data_manual_mapping(connect, list_map, list_plan_remove_unmap, \
-						list_camp_remove_unmap, list_plan_update, list_plan_insert_un_map, list_plan_insert_sum, is_manual_map)
+		# merge_data_manual_mapping.merger_data_manual_mapping(connect, list_map, list_plan_remove_unmap, \
+		# 				list_camp_remove_unmap, list_plan_update, list_plan_insert_un_map, list_plan_insert_sum, is_manual_map)
 		
 		time_update_manual = time.time() - update_manual
 		print("        Time update manual mapping to total : ", time_update_manual)
