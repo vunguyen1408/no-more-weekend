bcd4ed05f1e669a55bbdb8a888814aa1854c6532
japonikaa@gmail.com <japonikaa@gmail.com>
2017-11-10 14:15:40 +0700

    leth

--- 201610_MKT/fb_audit_content_video/rename_downloaded_video.mp.py
+++ 201610_MKT/fb_audit_content_video/rename_downloaded_video.mp.py
@@ -64,7 +64,7 @@ def do_work(list_index,in_queue, out_list):
                     subprocess.call(["mv", file_name,new_file_name])
                 else:
                     print('file exist',new_file_name)
-                    print("rm -f ", file_name)
+                    print("/bin/rm -f ", file_name)
                     subprocess.call(["/bin/rm -f", file_name])
 
 
